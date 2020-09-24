f04a9c5b09b8d0ce77fefb836b58a980bfc33dce
Mark <teamofmark@gmail.com>
2020-09-24 10:22:58 +0900

    while 진행중

--- JS/step3/js/1_while.js
+++ JS/step3/js/1_while.js
@@ -6,3 +6,39 @@
     정해진 횟수만큼의 반복은 for를 사용하지만, 무한반복처리, 파일 읽고쓰기 및 
     전송(java,php...), DB데이터 출력하기.
 */
+
+//*exp.1 패스워드 '1234'가 입력되면 '접속승인', 아니면 '잘못 입력하였습니다.'
+function pwCheck(){
+    while (true) {
+        var value = window.prompt("패스워드를입력하세요."); //? 이하 실행구문
+
+        if(value==1234){
+            alert('접속승인');
+            break;
+        }else{
+            alert('잘못 입력하였습니다.');
+        }
+    }
+}
+
+//* exp.2 while Continue
+function whileContinue(){
+    var i=1;
+    while(i<=10){ //? 
+        i++; //? i 
+        continue; //? 
+
+        document.write(i+"<br>");
+    }
+    document.write("최종 i = "+ i + "<br>"); //?
+
+//* exp.3 while Break
+function whileBreak(){
+    var i = 1;
+    while(i<=10){//? 
+        break;//? 
+        i++;
+        document.write(i+"<br>");
+    }
+    document.write("최종 i = "+ i + "<br>"); //? 
+}
