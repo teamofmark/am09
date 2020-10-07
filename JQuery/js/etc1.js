32b5f004094bde7910adc22abe31f68b6b9e12db
Mark <teamofmark@gmail.com>
2020-10-07 10:23:42 +0900

    jquery start

---
+++ JQuery/js/etc1.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var $divs = $("div");
+    $divs.css({
+        'color' : '#f00',
+        'border' : '5px solid black'
+    }); //? 함수-html,css연관 ('attrName','attrValue');
+    
+});
