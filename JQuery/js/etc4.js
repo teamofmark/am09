803ce949f61324ef624ee57b70e382e778646b87
Mark <teamofmark@gmail.com>
2020-10-13 09:49:50 +0900

    hori 완성 mix 진행중

--- JQuery/js/etc4.js
+++ JQuery/js/etc4.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     imgAlignHori();
+    imgAlignMix();
 });
 
 // *exp.1
@@ -9,5 +10,21 @@ function imgAlignHori(){
 
     $(".alignHori").click(function(){
         var imgLength = img.length;
+        var imgWidth = img.width();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = imgWidth * i;
+            image.css("left",xpos);
+        }
     });
+    $(".reset").click(function(){
+        img.css("left",0);
+    });
+}
+
+// *exp.2
+
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
 }
