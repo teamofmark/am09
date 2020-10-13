ae204924d9181048b9597d5ba7e85f02916e3f2d
Mark <teamofmark@gmail.com>
2020-10-13 09:26:37 +0900

    반복부 진행중

---
+++ JQuery/js/etc4.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    imgAlignHori();
+});
+
+// *exp.1
+function imgAlignHori(){
+    var img = $(".imgContainer img");
+    var xpos = 0;
+
+    $(".alignHori").click(function(){
+        var imgLength = img.length;
+    });
+}
