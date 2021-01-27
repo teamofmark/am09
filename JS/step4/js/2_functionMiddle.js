d112e8b0f1482c305c312cc519225e0e50ed714a
Mark <teamofmark@gmail.com>
2021-01-27 09:33:50 +0900

    계산기 callback 준비

--- JS/step4/js/2_functionMiddle.js
+++ JS/step4/js/2_functionMiddle.js
@@ -67,3 +67,66 @@ function calculator(op, numb1, numb2){
     ? add~div 함수들을 calculator 외부에서 사용할 수 없게 되는 대신 좀더 구조적인 코드로 접근할 수 있게 되는 것이기 때문에,
     ? 용도에 따라 맞게 사용해야 한다.
 */
+// * case.3 중첩 함수와 중첩함수를 포함한 함수의 관계
+function innerFunctionTest(){
+    var a = 10;
+    var b = 20;
+    var c = 30;
+    function outerFunc(){
+        var b = 200;
+        var c = 300;
+        function innerFunc(){
+            var c = 3000;
+
+            console.log('1. =' + a); //? 실행시 자신과 인접한 함수 내부부터 해당 변수를 찾는다. 전역 영역까지 존재 하지 않을 시 undefined.
+            console.log('2. =' + b);
+            console.log('3. =' + c);
+        }
+        innerFunc();
+    }
+    outerFunc();
+}
+
+// *case.4 callBack 함수를 이용한 사칙연산계산기
+
+function calculatorCallBack(op, numb1, numb2, callBack){
+    var result = '';
+
+    switch(op){
+        case "+": {
+            result = add(numb1, numb2);
+            break;
+        }
+        case "-": {
+            result = sub(numb1, numb2);
+            break;
+        }
+        case "*": {
+            result = mul(numb1, numb2);
+            break;
+        }
+        case "/": {
+            result = div(numb1, numb2);
+            break;
+        }
+        default: {
+            result = '잘못된 입력값입니다.';
+            break;
+        }
+    }
+    return result;
+    
+    function add(numb1, numb2){
+        return numb1 + numb2; 
+    }
+    function sub(numb1, numb2){
+        return numb1 - numb2;
+    }
+    function mul(numb1, numb2){
+        return numb1 * numb2;
+    }
+    function div(numb1, numb2){
+        return numb1 / numb2;
+    }
+    
+}
