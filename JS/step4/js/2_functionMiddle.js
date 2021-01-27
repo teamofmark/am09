be609f4f49cae42a6c881b73b4c61be9ad74c463
Mark <teamofmark@gmail.com>
2021-01-27 09:17:45 +0900

    중첩함수진행중

--- JS/step4/js/2_functionMiddle.js
+++ JS/step4/js/2_functionMiddle.js
@@ -13,3 +13,57 @@
     * 이 현상 을 클로저라 부르고, 이 현상을 일으키는 함수를 클로저함수 라고 한다.
     * 5. 멤버함수 - 멤버함수는 클래스 내부에 만들어지며, 메서드라 불린다. 
 */
+
+// !중첩함수
+// ? 지역변수처럼 함수 내부에서만 활용할 경우, 중복 코드나 코드그룹핑을 내부에서 처리해야 될 경우.
+
+// *case. 1 1초에 한번씩 'hi'출력.
+function sayHi(){
+    var count = 0;
+    setInterval(function(){
+        count ++;
+        document.write(count + '. hi <br>');
+    },1000);
+}
+
+// *case. 2 중첩함수를 이용해 사칙연산계산기
+function calculator(op, numb1, numb2){
+    var result = '';
+    switch(op){
+        case '+':
+            result = add(numb1, numb2);
+            break;
+        case "-" :
+            result = sub(numb1, numb2);
+            break;
+        case "*" :
+            result = mul(numb1, numb2);
+            break;
+        case "/" :
+            result = div(numb1, numb2);
+            break;
+        default:
+            result = "잘못된 입력값 입니다.";
+            break;
+    }
+
+    return result;
+
+    function add(numb1, numb2){
+        return numb1 + numb2; 
+    }
+    function sub(numb1, numb2){
+        return numb1 - numb2;
+    }
+    function mul(numb1, numb2){
+        return numb1 * numb2;
+    }
+    function div(numb1, numb2){
+        return numb1 / numb2;
+    }
+}
+/*
+    ? 이렇게 처리 할 경우 이전에 처리했던 (1_function.js todo.3)번과는 달리.
+    ? add~div 함수들을 calculator 외부에서 사용할 수 없게 되는 대신 좀더 구조적인 코드로 접근할 수 있게 되는 것이기 때문에,
+    ? 용도에 따라 맞게 사용해야 한다.
+*/
