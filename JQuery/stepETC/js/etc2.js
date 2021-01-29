6468aababab0aacea6c934a6bf0875036886e442
Mark <teamofmark@gmail.com>
2021-01-29 09:35:21 +0900

    exp.1 단순이동 완성

---
+++ JQuery/stepETC/js/etc2.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // *exp.1 원움직이기
+    moveCircle();
+});
+function moveCircle(){
+    var $circle = $('.circle');
+    $('#btnMoveCircle').click(function(){
+        var xpos = window.prompt('0부터 380이하의 숫자만 입력하세요.');
+        xpos = parseInt(xpos);
+        if(xpos >= 0 && xpos <= 380){
+            $circle.css('left',xpos);
+        }else{
+            alert('잘못된입력값입니다.');
+        }
+    });
+}
