57cf0d5b0452d84e7ee6ab20c9e8ec946de0735d
Mark <teamofmark@gmail.com>
2021-02-04 10:42:14 +0900

    bxslider controls Custom

---
+++ JQuery/bxSlider/js/bxsliderCustom.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(".bxslider").bxSlider({
+        mode: 'horizontal',
+        speed: 1000,
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        easing: 'ease-in',
+        captions: true,
+        adaptiveHeight: true,
+        adaptiveHeightSpeed: 500,
+        preventDefaultSwipeY: false,
+        pagerCustom: '.newPagerCustom',
+        controls: true,
+        prevSelector : '.btn_Prev',
+        nextSelector : '.btn_Next',
+        // prevText: '뒤로',
+        // nextText: '앞으로'
+        // pagerSelector: '.newPager'
+        // pagerType: 'short',
+        // pagerShortSeparator: '-'
+        // wrapperClass: 'wrap'
+        // ticker: true,
+        // tickerHover: true
+        // startSlide: 2,
+        // randomStart: false
+        // slideMargin: 50
+        // video: true
+        // ! touchEnabled . mobileChrome 충돌(a가 있을경우)
+    });
+});
