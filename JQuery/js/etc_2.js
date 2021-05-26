dca984c2015cca75721b4ebb0540c0971665ccd5
Mark <teamofmark@gmail.com>
2021-05-26 10:49:24 +0900

    keyControl 실습

--- JQuery/js/etc_2.js
+++ JQuery/js/etc_2.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 
     // *case. 2 원움직이기(xAxis, yAxis)
     detailMoveCircle();
+
+    // *case. 3 원움직이기(keyControl)
+    keyControl();
 });
 
 //  *case. 1
@@ -45,4 +48,53 @@ function detailMoveCircle(){
         }
     });
 
+}
+
+// *case. 3
+function keyControl(){
+    var circle = $('.circleKey');
+    var range = 50;
+    var currentXpos = 0;
+    var currentYpos = 0;
+
+    $(document).keydown(function(e){
+        console.log('입력한 키 코드 = ' + e.keyCode);
+        // ? top : w : 87, right : d : 68, bottom : s : 83, left : a : 65
+        switch(e.keyCode){
+            case 87:
+                currentYpos -= range;
+                break;
+            case 68:
+                currentXpos += range;
+                break;
+            case 83:
+                currentYpos += range;
+                break;
+            case 65:
+                currentXpos -= range;
+                break;
+            default:
+                console.log('잘못된키가 입력되었습니다!!');
+                break;
+        }
+        // todo. 못뚫고 나가게 하고, console로 메시지도 출력.
+        if(currentXpos < 0){
+            currentXpos = 0;
+            console.log('그라믄안대');
+        }
+        if(currentXpos > 380){
+            currentXpos = 380;
+            console.log('그라믄안대');
+        }
+        if(currentYpos < 0){
+            currentYpos = 0;
+            console.log('그라믄안대');
+        }
+        if(currentYpos > 380){
+            currentYpos = 380;
+            console.log('그라믄안대');
+        }
+        circle.css('left',currentXpos);
+        circle.css('top',currentYpos);
+    });
 }
