18e38f4aa800379e90fb6d2376ac876ca8e67435
Mark <teamofmark@gmail.com>
2021-05-31 10:47:37 +0900

    timeout 종료

---
+++ JQuery/js/etc_9.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var count = 0;
+    var $score = $('#score');
+    var $circle = $('#circle');
+    var playState = true;
+    
+    scoreCount(count, $score, $circle, playState);
+});
+function scoreCount(count, $score, $circle, playState){
+    $circle.click(function(){
+        if(playState == true){
+            count ++;
+            $score.text(count + '점');
+        }
+    });
+
+    setTimeout(function(){
+        playState = false;
+        alert('game over.');
+    }, 5000);
+}
