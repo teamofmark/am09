956e21ce050c9b037f575782fdd5b17d887fc663
Mark <teamofmark@gmail.com>
2021-06-02 09:38:11 +0900

    swapBanner random 함수 적용 답안

--- JQuery/js/etc_13.js
+++ JQuery/js/etc_13.js
@@ -1,7 +1,23 @@
-
+var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07.jpg","logo_08.jpg"];
+var currentIndex = 0;
+var speed = 0;
+var $banner1 = null;
+var $banner2 = null;
 
 $(document).ready(function(){
     init();
     swap($banner1, 1000); //? 배너전환 기능에 대상과 속도 매개변수로 전달.
     swap($banner2, 3000);
 });
+
+function init(){
+    $banner1 = $("#banner1");
+    $banner2 = $("#banner2");
+}
+function swap(selector, speed){
+    setInterval(function(){
+        var imgName = "images/" + logos[currentIndex];
+        currentIndex = Math.floor(Math.random()*logos.length);
+        selector.attr("src", imgName);
+    }, speed);
+}
