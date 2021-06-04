8f3bdd60a2e5e97f3489f1833922002cc130552f
Mark <teamofmark@gmail.com>
2021-06-04 09:56:27 +0900

    mobile 부분정리중

--- plugin/js/bxslider.js
+++ plugin/js/bxslider.js
@@ -20,7 +20,16 @@ $(document).ready(function(){
 
         easing: 'ease-in-out', //? default: null. slide전환 가속도 설정. CSS timing-function option 사용가능.
         captions: true, //? default: false. img's에 title attr을 부여하면 그 부분을 이미지 위로 보여준다.
-        video: true
+        video: true, //? default: false. video option 활성화. fitvids.js를 연결해두면 iframe,video등의 width,height 자동설정 가능.
+        responsive: true, //? default: true. slider 자체 반응형 켜기.
+        
+        // *mobile - 비중요.
+        touchEnabled: true, //? default: true. ? 터치스와이프 켜기 끄기.
+        swipeThreshold: 50, //? default: 50. ? slide 터치스와이프시. 터치상태로 움직여야하는 px수.
+        
+        // *비중요
+        preloadImages: 'visible', //? default: visible. slider시작전에 한장(첫장)만 load, all. slider 시작전에 slide모든 image download.
+        useCSS: true //? default: true. css animation(하드웨어가속)사용. 켜기(권장) 끄기.
         //? slideSelector : $(parent child) //? default: ''. slide로 작동될 요소를 직접선택 가능.
         
         //! carousel
