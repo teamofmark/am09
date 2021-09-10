a024667ada403fb62e214779be35ebb75d8084f3
Mark <teamofmark@gmail.com>
2021-09-10 10:41:53 +0900

    증감연산자 복합 todo 배포

--- JS/step1/js/1_4_operator.js
+++ JS/step1/js/1_4_operator.js
@@ -57,3 +57,50 @@ function numbOP2(){
     var result = a*b / 10;
     console.log(result);
 }
+// todo.1 숫자 5를 2로 나누었을 때 나머지 값이 몇인지 result 변수에 저장후 출력.
+function numbOP3(){
+    var result = 5%2;
+    console.log(result);
+}
+
+
+// todo.2 'hi'가 담긴 변수 a 와 'WEB'이 담긴 변수 b 를 합쳐 'hiWEB' 을 출력.
+function stringOP(){
+    var a = 'hi';
+    var b = 'WEB';
+    console.log(a + b);
+}
+
+// *case.3 ul, li 구조의 tag를 html page에 삽입 후 출력.
+function operatorEx(){
+    var list = '';
+    list += '<ul>';
+    list += '   <li>Hello</li>';
+    list += '   <li>JS..!</li>';
+    list += '</ul>';
+    document.body.innerHTML = list;
+}
+
+// *case.4 연산자가 뒤로 올경우 engine처럼 생각하기.
+function numberPlusAfter(){
+    var number = 10; //? 변수 선언 및 초기화.
+    alert(number++); //? 10을 출력후 더해서 다시 number로-
+    alert(number++); //? 11을 출력후 더해서 다시 number로- 
+    alert(number++); //? 12를 출력후 더해서 다시 number로-
+    console.log(number);
+}
+function numberPlusBefore(){
+    var number = 10;
+    alert(++number); //? 10에 1을 더해서(11) 출력후, 다시 number로 - 
+    alert(++number); //? 11에 1을 더해서(12) 출력후, 다시 number로 -
+    alert(++number); //? 12에 1을 더해서(13) 출력후, 다시 number로 -
+    console.log(number);
+}
+function numberPlusTodo(){
+    var number = 10; // ? 변수 선언 및 초기화
+    alert(number++); //? 출력 : 몇, 현재 : 몇 추가설명 가능?
+    alert(--number); //? 출력 : 몇, 현재 : 몇 추가설명 가능?
+    alert(++number); //? 출력 : 몇, 현재 : 몇 추가설명 가능?
+    alert(number--); //? 출력 : 몇, 현재 : 몇 추가설명 가능? 
+    console.log(number); // ?최종 몇
+}
