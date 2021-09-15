ddb8239e2f85edc504a7cd6d21d36969dea95c6e
Mark <teamofmark@gmail.com>
2021-09-15 10:51:05 +0900

    whileloop todo 배포

--- JS/step3/js/1_while.js
+++ JS/step3/js/1_while.js
@@ -6,3 +6,26 @@
     정해진 횟수만큼의 반복은 for를 사용하지만, 무한반복처리, 파일 읽고쓰기 및 
     전송(java,php...), DB데이터 출력하기.
 */
+// *case. 1 password '1234가 입력되면 '접속승인' 그게 아니라면 '잘못 입력하였습니다. 다시입력해주세요' 반복
+function pwCheck(){
+    while(true){
+        var value = window.prompt("패스워드를 입력하세요.");
+
+        if(value == 1234){
+            alert("접속승인");
+            break;
+        }else{
+            alert("패스워드가틀렸습니다. 다시 입력해주세요.");
+        }
+    }
+}
+// *case.2 while Break
+function whileBreak(){
+    var i = 1;
+    while(i <= 10){
+        i++;
+        document.write(i + '<br>');
+    }
+    document.write('최종 i = ' + i + '<br>');
+
+} 
