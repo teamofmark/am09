374fa426f8204148390b6600857f5d8d056e9ab1
Mark <teamofmark@gmail.com>
2021-09-29 10:18:28 +0900

    선택자부 종료

--- JQuery/js/etc1.js
+++ JQuery/js/etc1.js
@@ -2,4 +2,30 @@ $(document).ready(function(){
     // *case.1 
     var $divs = $("div");
     $divs.css("color","#f00");
+
+    // *case.2
+    $("#btnCheck").click(sayHello);
+
+    // *case.3
+    $("#btnAddBorder").click(addBorder);
+
+    // todo
+    $("#btnTextChange").click(textChange);
 });
+// *case.2
+function sayHello(){
+    alert('hello');
+}
+// *case.3
+function addBorder(){
+    $("#panel").css("border","5px solid black");
+}
+// todo
+function textChange(){
+    $("#panel2").css({ //? 선택자에서 nth 사용시 주의사항. odd와 even이 반대.
+        'font-size' : '25px',
+        'color' : 'green',
+        'font-weight' : 'bold',
+        'border': '5px solid green'
+    });
+}
