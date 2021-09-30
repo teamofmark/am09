e07a4fd0d62992bf0033783c26bad9f32ae3f96c
Mark <teamofmark@gmail.com>
2021-09-30 09:54:32 +0900

    case2 함수구조분리

--- JQuery/js/etc2.js
+++ JQuery/js/etc2.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     moveCircle();
-    detailMoveCircle();
+    // detailMoveCircle();
+    detailMoveCircleExt();
 });
 
 function moveCircle(){
@@ -28,6 +29,38 @@ function detailMoveCircle(){
         // ? xpos, ypos는 0보다 크거나 같고 380보다 작거나 같을때 구동하는 if. 그외 나머지 error
         // ? xpos, ypos는 0보다 작거나 380보다 클때 구동하는 if. 그외 나머지 실행.
         // todo.
-        
+        if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+            alert('잘못된 수치입니다. 0 ~ 380이내로 입력하세요.');
+        }else{
+            $circle.css({
+                'left' : xpos,
+                'top' : ypos
+            });
+        }
     });
+}
+function detailMoveCircleExt(){
+    var $circle = null;
+    circleInit();
+    $("#btnDetailMove").click(circleEvent);
+}
+function circleInit(){
+    $circle = $(".circleDetail");
+}
+function circleEvent(){
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+    circleMovCommand(xpos, ypos);
+}
+function circleMovCommand(xpos, ypos){
+    if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+        alert("잘못된 수치입니다. 0 ~ 380이내로 입력하세요.");
+    }else{
+        $circle.css({
+            'left' : xpos,
+            'top' : ypos
+        });
+    }
 }
