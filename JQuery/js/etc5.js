ee5a57ec1ef74c67f0f82d2040e9ba48493effef
Mark <teamofmark@gmail.com>
2021-10-01 10:51:02 +0900

    원상태로돌리기 todo

---
+++ JQuery/js/etc5.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    imgAlignCustom(4); //? 정렬하고자 하는 단수를 매개변수로 입력.
+});
+function imgAlignCustom(count){
+    var img = $(".imgContainerCustom img");
+    var xpos = 0, ypos = 0;
+    
+    $(".alignImg").click(function(){
+        var imgLength = img.length; //? loop의 횟수
+        var imgWidth = img.width(); //? img 너비구하기
+        var imgHeight = img.height(); //? img 높이구하기
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i % count)*imgWidth; //? 0,1,2,0,1,2,0,1,2
+            ypos = parseInt(i / count)*imgHeight; //? 0,0,0, 1,1,1 2,2,2 3,3,3....
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetImg").click(function(){
+        // todo. 원상태로 돌리기
+    });
+}
