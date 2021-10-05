fabd3efb3c8449400990aae62753b7e7361b0eea
Mark <teamofmark@gmail.com>
2021-10-05 09:18:28 +0900

    ETC10 동작구현완성

--- JQuery/js/etc10.js
+++ JQuery/js/etc10.js
@@ -10,7 +10,15 @@ $(document).ready(function(){
 function startMove($circle, runStep, xpos, railWidth, timerID){
     $("#btn_start").click(function(){
         timerID = setInterval(function(){
+            xpos += runStep;
+            $circle.css('left',xpos);
 
+            if(xpos > railWidth || xpos < 0){
+                runStep *= -1;
+            }
         },0);
+    });
+    $("#btn_stop").click(function(){
+        clearInterval(timerID);
     });
 }
