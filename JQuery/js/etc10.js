28a125634ae02ad38b9978609b11952ae0088e4a
Mark <teamofmark@gmail.com>
2021-10-05 09:27:52 +0900

    함수구조분리

--- JQuery/js/etc10.js
+++ JQuery/js/etc10.js
@@ -1,24 +1,66 @@
+// $(document).ready(function(){
+//     var $circle = $("#circle");
+//     var runStep = 10;
+//     var xpos = $circle.position().left;
+//     var railWidth = $("#rail").width();
+    // var timerID = 0;
+
+//     startMove($circle, runStep, xpos, railWidth, timerID);
+// });
+// function startMove($circle, runStep, xpos, railWidth, timerID){
+//     $("#btn_start").click(function(){
+//         timerID = setInterval(function(){
+//             xpos += runStep;
+//             $circle.css('left',xpos);
+
+//             if(xpos > railWidth || xpos < 0){
+//                 runStep *= -1;
+//             }
+//         },0);
+//     });
+//     $("#btn_stop").click(function(){
+//         clearInterval(timerID);
+//     });
+// }
+
+var $circle = null;
+var railWidth = null;
+var runStep = 10;
+var timerID = 0;
+
 $(document).ready(function(){
-    var $circle = $("#circle");
-    var runStep = 10;
-    var xpos = $circle.position().left;
-    var railWidth = $("#rail").width();
-    var timerID = 0;
-
-    startMove($circle, runStep, xpos, railWidth, timerID);
+    init(); //? 대상 및 변수 불러오기
+    initEvent();
 });
-function startMove($circle, runStep, xpos, railWidth, timerID){
+function init(){
+    $circle = $("#circle");
+    railWidth = $("#rail").width();
+}
+function initEvent(){
     $("#btn_start").click(function(){
-        timerID = setInterval(function(){
-            xpos += runStep;
-            $circle.css('left',xpos);
+        moveStart();// ? 움직이는 기능
+    });
 
-            if(xpos > railWidth || xpos < 0){
-                runStep *= -1;
-            }
-        },0);
-    });
     $("#btn_stop").click(function(){
-        clearInterval(timerID);
+        moveStop();
     });
+}
+function moveStart(){
+    if(timerID == 0){
+        timerID = setInterval(function(){
+            moveCircle();
+        },0);
+    }
+}
+function moveCircle(){
+    var xpos = $circle.position().left;
+    xpos += runStep;
+    $circle.css("left",xpos);
+    if(xpos > railWidth || xpos < 0){
+        runStep *= -1;
+    }
+}
+function moveStop(){
+    clearInterval(timerID);
+    timerID = 0;
 }
