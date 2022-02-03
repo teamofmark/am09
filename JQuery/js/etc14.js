5ece71fe04b3195c37b29cdddf52d0e2888d8d53
Mark <teamofmark@gmail.com>
2022-02-03 10:26:55 +0900

    todo / 구조 및 공식배포

---
+++ JQuery/js/etc14.js
@@ -0,0 +1,7 @@
+/*
+    ? random position 공식
+    * $circle.css({
+    *     left: Math.floor(Math.random()*($panel.width() - $circle.width())),
+    *     top: Math.floor(Math.random()*($panel.height() - $circle.height()))
+    * });
+*/
