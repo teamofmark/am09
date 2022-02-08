6bf38ed8ff40c0721efec9b353587214bdcb526b
Mark <teamofmark@gmail.com>
2022-02-08 09:37:08 +0900

    autoControlsSelector

--- plugin/bxslider/js/slider.js
+++ plugin/bxslider/js/slider.js
@@ -26,6 +26,26 @@ $(document).ready(function(){
         pager: true, //? default: true. pager 켜기 끄기.
         //? pagerType: 'full',  default: full.(circle). short: count로 변경
         //? pagerSelector: '.newPager',  default: ''. 공간 생성 및 class 부여 하면 pager 뼈대제공.
-        pagerCustom: '.customPager'
+        pagerCustom: '.customPager',  //? default: null. pager 구조 직접 작성 및 data-slide-index="i"를 설정하면 원하는 style의 pager 구현가능.
+
+        // *controler
+        controls: true, ///? default: true. control button 켜기 끄기.
+        //? nextSelector: '.btn_next', 공간 생성 및 class 부여 하면 controler 다음버튼 뼈대 제공. 
+        //? prevSelector: '.btn_prev', 공간 생성 및 class 부여 하면 controler 이전버튼 뼈대 제공.
+        //? nextText: '다음', default: 'Next'생성된 뼈대 안에 text 변경
+        //? prevText: '이전', default: 'Prev'생성된 뼈대 안에 text 변경
+
+        // *auto
+        auto: true, //? default: false. slide 자동전환 켜기. false로 설정시 autoControls에서 작동가능.
+        stopAutoOnClick: false, //? default: false. control button을 클릭 하였을 때 자동전환 중지
+        pause: 4000, //? default: 4000(m/s). 자동전환시 멈추는 시간.
+        autoStart: true, //? default: true. 화면 준비가 완료되면 바로 자동전환 시작.
+        autoDelay: 1500, //? default: 0(m/s). 자동시작 전 지연시간 설정
+        autoHover: true, //? default:  false. onMouseOver상태일 때 자동전환 중지.
+        autoControls: true, //? default: false. 시각장애인들을 배려 하는것.
+        autoControlsCombine: true, //? default: false. 재생중일때 '중지'만, 중지중일때 '시작'만 보이게.
+        // ? autoControlsSelector: '.clasName', default: null. 공간 생성 및 class에 autoControl 뼈대 제공.
+        // ? startText : '시작', default: 'Start' 생성된 뼈대 안에 text 변경
+        // ? stopText : '중지', default: 'Stop' 생성된 뼈대 안에 text 변경
     });
 });
