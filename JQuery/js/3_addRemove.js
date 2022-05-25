c1f1348907baf4357cee295ee5ffd718c315d274
Mark <teamofmark@gmail.com>
2022-05-25 10:11:55 +0900

    style 적용및 js 추가

---
+++ JQuery/js/3_addRemove.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    toggleCheck();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    var checkStatus = false;
+}
