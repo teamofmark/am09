43940a7b39991f51d19e749f46dd56c43ce475df
Mark <teamofmark@gmail.com>
2022-05-25 10:23:36 +0900

    passwordInput Custom

--- JQuery/js/3_addRemove.js
+++ JQuery/js/3_addRemove.js
@@ -5,4 +5,13 @@ function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
     var $checkLabel = $(".label.checkLabel");
     var checkStatus = false;
+
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        if(checkStatus == true){
+            $checkTarget.text("check_box"); //? text를 가져오거나 매개변수로 값을 넣어 바꾸기.
+        }else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+    });
 }
