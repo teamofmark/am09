a01837ba9485f2e06ad9bea7e1a90ba4d79d1188
Mark <teamofmark@gmail.com>
2022-05-26 10:40:14 +0900

    foldUI 완성

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     // *case.1
     imgAlignHori();
+
+    // todo
+    imgAlignMix();
 });
 
 function imgAlignHori(){
@@ -18,5 +21,33 @@ function imgAlignHori(){
     });
     $(".reset").click(function(){
         img.css("left", 0);
+    });
+}
+
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0, ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        // var imgHeight = img.height();
+        
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3)*imgWidth; //? 0 % 3 = 0, 1 % 3 = 1, 2 % 3 = 2, 3 % 3 = 0......* imgWidth 
+            ypos = parseInt(i/3)*imgWidth; //? 0 ? 3 = 0, 1 ? 3 = 0, 2 ? 3 = 0, 3 ? 3 = 1
+
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        });
     });
 }
