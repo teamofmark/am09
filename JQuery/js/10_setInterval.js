906203b690d99291b11a3e306e9e8d2035829c3d
Mark <teamofmark@gmail.com>
2022-05-30 10:45:44 +0900

    함수분리없이 완성

--- JQuery/js/10_setInterval.js
+++ JQuery/js/10_setInterval.js
@@ -13,12 +13,16 @@ function startMove($circle, runStep, xpos, railWidth, timerID){
         timerID = setInterval(function(){
             xpos += runStep;
             $circle.css("left",xpos);
-
-            // todo. 현재 xpos는 증가만하는데. 왕복운동을 만드려면 어찌해야 하는가?
-            if(){
-                
+            if(xpos > railWidth || xpos < 0){
+                runStep *= -1;
             }
-
         },0);
+        stopMove(timerID);
+    });
+}
+
+function stopMove(timerID){
+    $("#btn_stop").click(function(){
+        clearInterval(timerID);
     });
 }
