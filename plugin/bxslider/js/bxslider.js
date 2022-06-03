32b8f4cb28934cf71666030778f8fd36c3eb0049
Mark <teamofmark@gmail.com>
2022-06-03 10:47:57 +0900

    bxslider configuration Options 진행중

--- plugin/bxslider/js/bxslider.js
+++ plugin/bxslider/js/bxslider.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".slider").bxSlider({
-        mode: 'vertical'
+        mode: 'horizontal', //? default : horizontal(수평) / vertical(수직) / fade(in,out)
+        speed: 750 //? m/s default : 500. 전환속도제어
     });
 });
