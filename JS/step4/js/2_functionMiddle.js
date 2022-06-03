4cf255ee248d2d6e1a0eadcffb1179ac380a89cc
Mark <teamofmark@gmail.com>
2022-06-03 09:15:06 +0900

    callback 함수를 이용한 계산기

--- JS/step4/js/2_functionMiddle.js
+++ JS/step4/js/2_functionMiddle.js
@@ -42,3 +42,48 @@ function innerFunctionTest(){
     }
     outerFunc();
 }
+
+// *case.2 callBack 함수를 이용한 사칙연산 계산기 만들기.
+ function calculatorCallBack(op,numb1,numb2,callBack){
+    var result = '';
+    switch(op){
+        case "+":{
+            result = add(numb1, numb2);
+            break;
+        }
+        case "-":{
+            result = sub(numb1, numb2);
+            break;
+        }
+        case "*":{
+            result = mul(numb1, numb2);
+            break;
+        }
+        case "/":{
+            result = div(numb1, numb2);
+            break;
+        }
+        default:{
+            result = "잘못된 입력값 입니다.";
+            break;
+        }
+    }
+    function add(numb1,numb2){
+        return numb1 + numb2;
+    }
+    function sub(numb1,numb2){
+        return numb1 - numb2;
+    }
+    function mul(numb1,numb2){
+        return numb1 * numb2;
+    }
+    function div(numb1,numb2){
+        return numb1 / numb2;
+    }
+
+    callBack(result);
+ }
+
+ function addPrint(value){
+     document.write('두 수의 합은' + value);
+ }
