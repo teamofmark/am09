942850c5e39204a6f10b6e7f5c5d03d2b69aac60
Mark <teamofmark@gmail.com>
2022-09-15 10:53:54 +0900

    if 시작

--- JS/step2/js/1_if.js
+++ JS/step2/js/1_if.js
@@ -15,3 +15,15 @@
 	alert('종료');
     
 */
+// *case.1 현재시간이 오후인가 오전인가
+function amORpm(){
+    var date = new Date();
+    var hour = date.getHours(); //? 0 ~ 24시 로전달
+    // ? 변수 선언부
+    if(hour < 12){
+        alert('오전입니다.');
+    }
+    if(hour >= 12){
+        alert('오후입니다.');
+    }
+}
