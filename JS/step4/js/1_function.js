3fe10b988a660f86094ee1e17813151c76bb89bd
Mark <teamofmark@gmail.com>
2022-09-23 10:51:44 +0900

    주석 에러 수정

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -87,7 +87,7 @@ function infiniteSum(){
     document.write("총 " + count + "번 실행함.");
 }
 // todo. 다음 실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 함수 만들기.
-
+/*
     //* 실행구문
     document.write("1 결과 =" + calculator("+", 20, 10));
     document.write("2 결과 =" + calculator("-", 20, 10));
