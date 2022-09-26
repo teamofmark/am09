3af3dad30d3132ed29d72b1cba0e8d28992f792f
Mark <teamofmark@gmail.com>
2022-09-26 09:22:55 +0900

    계산기 함수구조분리

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -127,3 +127,47 @@ function calculator(op, numb1, numb2){
     return result;
     // ? 처리된 변수 (결과를 담을 변수) 내보내야지.(?)
 }
+
+// todo. 위의 예제에 추가로 사칙연산 부분을 함수로 변환하여 보다 편하게 사용 할 수 있게 만들기.
+/*
+//* 실행구문
+document.write(calculator2("+", 20, 10) + "<br>");
+document.write(add(20, 10) + "<br>");
+document.write(sub(20, 10) + "<br>");
+document.write(mul(20, 10) + "<br>");
+document.write(div(20, 10) + "<br>");
+*/
+
+function calculator2(op, numb1, numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = add(numb1,numb2);
+            break;
+        case "-":
+            result = sub(numb1,numb2);
+            break;
+        case "*":
+            result = mul(numb1,numb2);
+            break;
+        case "/":
+            result = div(numb1,numb2);
+            break;
+        default:
+            result = "잘못된 연산자 입니다.";
+            break;
+    }
+    return result;
+}
+function add(numb1, numb2){
+    return numb1 + numb2;
+}
+function sub(numb1,numb2){
+    return numb1 - numb2;
+}
+function mul(numb1,numb2){
+    return numb1 * numb2;
+}
+function div(numb1,numb2){
+    return numb1 / numb2;
+}
