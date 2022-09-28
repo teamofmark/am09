e0d9d24e2fd8350093a169a06b0ccb9289594a45
Mark <teamofmark@gmail.com>
2022-09-28 09:54:47 +0900

    함수구조분리 (3단)

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
     // todo. x,y축으로 움직이기
     // $("#btnDetailMove").click(detailMoveCircle);
-    $("#btnDetailMove").click(circleEvent);
+    // $("#btnDetailMove").click(circleEvent);
+    detailMoveCircleExt();
 
 });
 
@@ -20,39 +21,64 @@ function moveCircle(){
     }
 }
 
-function detailMoveCircle(){
-    var $circle = $(".circleDetail");
-    var xpos = $("#xpos").val();
-    var ypos = $("#ypos").val();
+// function detailMoveCircle(){
+//     var $circle = $(".circleDetail");
+//     var xpos = $("#xpos").val();
+//     var ypos = $("#ypos").val();
     
-    xpos = parseInt(xpos);
-    ypos = parseInt(ypos);
+//     xpos = parseInt(xpos);
+//     ypos = parseInt(ypos);
 
-    if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
-        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
-    }else{
-        $circle.css({
-            "left" : xpos,
-            "top" : ypos
-        });
-    }
+//     if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+//         alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+//     }else{
+//         $circle.css({
+//             "left" : xpos,
+//             "top" : ypos
+//         });
+//     }
+// }
+// function circleEvent(){
+//     var xpos = $("#xpos").val(); //? 해당 아이디의 value를 가져오기
+//     var ypos = $("#ypos").val(); //? 해당 아이디의 value를 가져오기
+//     xpos = parseInt(xpos);
+//     ypos = parseInt(ypos);
+//     circleMoveCommand(xpos,ypos); //? circleMoveCommand 함수를 실행하라. 단 xpos, ypos 형변환된 인자를 넣어라.
+// }
+// function circleMoveCommand(xpos,ypos){
+//     var $circle = $(".circleDetail");
+//     if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
+//         alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+//     }else{
+//         $circle.css({
+//             "left" : xpos,
+//             "top" : ypos
+//         });
+//     }
+// }
+
+function detailMoveCircleExt(){
+    var $circle = null;
+    circleInit();
+    $("#btnDetailMove").click(circleEvent);
 }
-
+function circleInit(){
+    $circle = $(".circleDetail");
+}
 function circleEvent(){
     var xpos = $("#xpos").val();
     var ypos = $("#ypos").val();
     xpos = parseInt(xpos);
     ypos = parseInt(ypos);
-    circleMoveCommand(xpos,ypos);
+    circleMovCommand(xpos, ypos);
 }
-function circleMoveCommand(xpos,ypos){
-    var $circle = $(".circleDetail");
+function circleMovCommand(xpos,ypos){
     if(xpos > 380 || ypos > 380 || xpos < 0 || ypos < 0){
-        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+        alert('잘못되었다.');
     }else{
         $circle.css({
             "left" : xpos,
             "top" : ypos
-        });
+        })
     }
 }
