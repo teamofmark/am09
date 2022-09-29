293a47f19d44acc10163d5bcdbdc6f2102a05d99
Mark <teamofmark@gmail.com>
2022-09-29 10:18:06 +0900

    선택적기호 비밀번호 만들기

--- JQuery/js/3_addRemove.js
+++ JQuery/js/3_addRemove.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     toggleCheck();
     // todo. toggleRadio 만들어보기
-    toggleRadio();
+    toggleTodo();
 });
 function toggleCheck(){
     var $checkLabel = $(".label.checkLabel");// ? 시키는놈. -> button, input, click, keydown....
@@ -20,6 +20,18 @@ function toggleCheck(){
 
 
 }
-function toggleRadio(){
-    
+function toggleTodo(){
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text("visibility");
+            $("#userPW").attr("type","text");
+        }else{
+            $toggleTarget.text("visibility_off");
+            $("#userPW").attr("type","password");
+        }
+    });
 }
