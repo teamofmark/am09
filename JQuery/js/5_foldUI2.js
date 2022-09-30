7b4e117209674249193598764ac4af3cb878ced8
Mark <teamofmark@gmail.com>
2022-09-30 10:04:14 +0900

    문제풀이

--- JQuery/js/5_foldUI2.js
+++ JQuery/js/5_foldUI2.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
-    imgAlignCustom(6); //? 매개변수로 넣은 숫자형태(n)로 작동할 수 있도록.
+    imgAlignCustom(2); //? 매개변수로 넣은 숫자형태(n)로 작동할 수 있도록.
 });
 
-function imgAlignCustom(){
-    
+function imgAlignCustom(count){
+    var img = $(".imgContainerCustom img");
+    var xpos = 0;
+    var ypos = 0;
+
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        var imgHeight = img.height();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%count)*imgWidth;
+            ypos = parseInt(i/count)*imgHeight;
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        });
+    });
 }
