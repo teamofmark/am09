9f76d1377b08c04f0f2e1547fdab98ae15380af7
Mark <teamofmark@gmail.com>
2022-10-04 10:52:39 +0900

    attrControl init 까지 완료

---
+++ JQuery/js/11_attrControl.js
@@ -0,0 +1,11 @@
+var currentIndex = 0;
+var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07.jpg","logo_08.jpg"];
+var $banner = null;
+
+$(document).ready(function(){
+    init();
+});
+
+function init(){
+    $banner = $(".banner");
+}
