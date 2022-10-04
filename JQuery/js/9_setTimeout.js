2fec296ccdd41cfb800577a459022ead53463150
Mark <teamofmark@gmail.com>
2022-10-04 09:26:30 +0900

    timeout = 특정시간 이후 한번만 실행

--- JQuery/js/9_setTimeout.js
+++ JQuery/js/9_setTimeout.js
@@ -13,4 +13,9 @@ function scoreCount(count, $score, $circle, playState){
             $score.text(count + "점");
         }
     });  
+
+    setTimeout(function(){
+        alert("게임종료.");
+        playState = false;
+    },10000); //? 고정인자 2개(실행함수, 시간(m/s));
 }
