77f57c298f06353ed385c17c3d816c362a5041ac
Mark <teamofmark@gmail.com>
2022-10-05 09:20:02 +0900

    기본 img swap

--- JQuery/js/11_attrControl.js
+++ JQuery/js/11_attrControl.js
@@ -4,8 +4,22 @@ var $banner = null;
 
 $(document).ready(function(){
     init();
+    setInterval(swap,1000);
 });
 
 function init(){
     $banner = $(".banner");
+}
+function swap(){
+    var imgName = "images/" + logos[currentIndex];//? path - server상에서 경로를 의미함.
+    $banner.attr("src",imgName); 
+    //? arguments - 1. attrName만 기재할 경우 = 해당 attrName의 value만 가져오라.
+    //? 2. attrName,attrVal를 같이 입력할 경우 = 해당 attrName의 value를 바꿔라.
+    currentIndex ++;
+    reset();
+}
+function reset(){
+    if(currentIndex == logos.length){
+        currentIndex = 0;
+    }
 }
