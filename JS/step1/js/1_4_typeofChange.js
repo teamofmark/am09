07a82d88112c311456a6a45efbfe6da2d2fa3d96
Mark <teamofmark@gmail.com>
2023-01-13 10:47:06 +0900

    과제 문제배포

--- JS/step1/js/1_4_typeofChange.js
+++ JS/step1/js/1_4_typeofChange.js
@@ -11,3 +11,95 @@
     ? 명시적 형변환
     * method 나 lib Core func를 이용하여 강제로 숫자 -> 문자 로 변환.
 */
+// *case. 1 자신의 나이를 입력받고 20을 더한 값 출력.
+function agePlus(){
+    var age = window.prompt('나이입력');
+    var result = parseInt(age) + 20;
+    console.log(result);
+}
+// *case.2 강제 숫자 -> 문자형변환
+function forcedString(){
+    var a = '30';
+    var result = 1 + a + 10;
+    console.log(result);
+}
+// *case.3 강제 불리언 -> 숫자형변환
+function forcedNumber(){
+    var result = 2 + true; //? true = 1, false = 0.
+    console.log(result);
+}
+// *case.4 강제 불리언 -> 문자형변환
+function forcedString2(){
+    var a = '2' + true;
+    console.log(a);
+}
+// *case.5 test1에 들어있는 숫자를 문자로 명시적 형변환.
+/*
+    ? String (전역함수, 객체..)
+    * String(value); - 문자형으로 변환
+
+    ? .toString()
+    * NumberTarget.toString(); - 문자형(진수)변환
+
+    ? .toFixed()
+    * Number.toFixed(); - 소수점자리수(반올림발생) > 실수문자형변환
+*/
+function stringNumb(){
+    var test1 = 15;
+    var result = String(test1);
+    console.log(typeof(result) + ' = ' + result);
+}
+function stringtoNumb(){
+    var test1 = 15;
+    var result = test1.toString();
+    console.log(typeof(result) + ' = ' + result);
+}
+
+// *case.6 test에 들어있는 문자를 숫자로 형변환.
+/*
+    ? Number
+    * Number(value); - 정수,실수 구분없이 형변환.
+
+    ? parse
+    * 1. parseInt(value); - 정수형변환.
+    * 2. parseFloat(value); - 실수형변환.
+*/
+function numbString(){
+    var test = '300';
+    var result = parseInt(test) + 10;
+    console.log('result = ' + result);
+}
+// todo.1 변수 test에 들어있는 숫자 100을 문자형으로 변형. result 로 출력(typeof 검수필요).
+
+
+// todo. 1-1 암시적형변환
+function todo1(){
+    var test = 100;
+    var result = test; // todo. 암시적으로 형변환시키기
+    console.log(); //todo. typeof로 자료형 검수 및 출력.
+}
+// todo. 1-2 String 형변환
+function todo2(){
+    var test = 100;
+    var result = test; // todo. 전역함수String으로 형변환시키기
+    console.log(); //todo. typeof로 자료형 검수 및 출력.
+}
+// todo. 1-3 toString 형변환
+function todo3(){
+    var test = 100;
+    var result = test; // todo. tosTring으로 형변환시키기
+    console.log(); //todo. typeof로 자료형 검수 및 출력.
+}
+// todo.2 문자 형을 숫자 형으로 변환하여 연산이 가능하게 만들기.
+// todo. 2-1 parse 형변환
+function todo4(){
+    var test = "100.51";
+    var result = test + 150 + "20"; // todo. parse이용한 형변환
+    console.log(); //todo. typeof로 자료형 검수 및 출력.
+}
+// todo. 2-2 Number 형변환
+function todo5(){
+    var test = "100.51";
+    var result = test + 150 + "20"; // todo. 전역함수 Number를 이용한 형변환
+    console.log(); //todo. typeof로 자료형 검수 및 출력.
+}
