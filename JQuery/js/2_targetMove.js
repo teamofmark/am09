5b4872ac3e2080efe397f02953d528c06825e864
Mark <teamofmark@gmail.com>
2023-01-27 10:04:38 +0900

    2번예제 풀이

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -19,4 +19,15 @@ function detailMoveCircle(){
     var xpos = $("#xpos").val(); //? .val() -> 현재담긴"값"을불러오기 / .val("blabla") -> 현재담긴값을 "blabla"로 바꿔라.
     var ypos = $("#ypos").val();
     // todo. 형변환 -> 조건처리 -> 움직임.
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+
+    if(xpos <= 380 && ypos <= 380 && xpos >= 0 && ypos >= 0){
+        $circle.css({
+            "left": xpos,
+            "top" : ypos
+        });
+    }else{
+        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+    }
 }
