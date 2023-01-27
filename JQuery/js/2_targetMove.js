73080ecce4d4575b0348ad4663ffeaa4479d6974
Mark <teamofmark@gmail.com>
2023-01-27 09:33:30 +0900

    1번문제

---
+++ JQuery/js/2_targetMove.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#btnMoveCircle").click(moveCircle);
+});
+function moveCircle(){
+    var $circle = $(".circle");
+    var xpos = window.prompt("0부터 380이하의 숫자만 입력하세요.");
+    xpos = parseInt(xpos);
+
+    if(xpos >= 0 && xpos <= 380){
+         $circle.css("left",xpos);
+    }else{
+        alert("잘못된 수치입니다.");
+    }
+    
+}
