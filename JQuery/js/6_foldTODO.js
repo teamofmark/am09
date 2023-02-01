2a47e151b95f59b7fd8f3f10aa2e18951763d062
Mark <teamofmark@gmail.com>
2023-02-01 09:21:47 +0900

    힌트 첨부

---
+++ JQuery/js/6_foldTODO.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    gallery(); //? 초기식실행
+    //? 1열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 2열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 3열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 4열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 5열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 6열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 7열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 8열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 9열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+    //? 10열 버튼을 click 하였을 때 위 gallery함수에 인자전달 및 수행
+});
+function gallery(){ //? 매개변수로 몇개를 어떤것을 전달해야 하는가?
+
+}
