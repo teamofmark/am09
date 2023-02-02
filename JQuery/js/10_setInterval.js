9fa03312938571fe9cfbf1a075e919d1e78283c3
Mark <teamofmark@gmail.com>
2023-02-02 09:37:38 +0900

    setInterval 진행중

---
+++ JQuery/js/10_setInterval.js
@@ -0,0 +1,33 @@
+var $circle = null;
+var railWidth = null;
+var runStep = 10;
+var timerID = 0;
+
+$(document).ready(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $circle = $("#circle");
+    railWidth = $("#rail").width();
+}
+function initEvent(){
+    $("#btn_start").click(moveStart); //? 움직이는 기능
+    $("#btn_stop").click(moveStop); //? 멈추는 기능
+}
+function moveStart(){
+    if(timerID == 0){
+        timerID = setInterval(moveCircle,1);
+    }
+}
+function moveCircle(){
+    var xpos = $circle.position().left; //? 현재 position : left 불러오기
+    xpos += runStep;
+    $circle.css("left",xpos);
+    // todo. 원이 frame 내에 갇히려면?
+    
+}
+
+function moveStop(){
+
+}
