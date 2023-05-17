d6b13deaf338b355bc9933852e179e52fe1e0710
Mark <teamofmark@gmail.com>
2023-05-17 10:30:36 +0900

    todo. source제공

--- JS/step2/js/1_if.js
+++ JS/step2/js/1_if.js
@@ -44,13 +44,48 @@ function lastDay(){
 // todo. 아침 먹을시간? 점심 먹을시간? 저녁 먹을시간?
 function mealTime(){
     var date = new Date();
-    var hour = ?;
+    var hour = date.getHours();
 
-    if(){//? 아침먹을시간 - 11시 보다 작으면 아침
-
-    }else if(){//? 점심먹을시간 - 그것이 아니고 15시보다 현시각이 작으면 점심.
-
+    if(hour < 11){//? 아침먹을시간 - 11시 보다 작으면 아침
+        alert('아침먹을시간 - 많이먹어');
+    }else if(hour < 15){//? 점심먹을시간 - 그것이 아니고 15시보다 현시각이 작으면 점심.
+        alert('점심먹을시간 - 적당히먹어');
     }else{ //? 그것도 아니라면 저녁.
-
+        alert('저녁먹을시간 - 먹을거야?');
     }
+}
+// todo. 홀수 짝수 구분해주는 조건문
+function oddOrEven(){
+    var numb = window.prompt('숫자를 입력하세요.','홀,짝을 구분해드립니다.');
+
+    // ? 홀수의 조건? 짝수의 조건? - 수의 시작이 홀,짝? 0,1 <- 2의 규칙을 갖는 수들은 나누었을때 나머지가 0. 그것의 1이 더해지는 수들은 나머지가 무조건 1이된다.  
+    if(numb%2 == 0){
+        alert('짝수입니다.');
+    }else if(numb%2 == 1){
+        alert('홀수입니다.');
+    }else{
+        alert('값 똑바로 넣어라.');
+    }
+}
+// *case. 3 양,음수 판별식
+function numbJudgment(){
+    var numb = prompt('숫자를 입력하세요.','양,음수 상관없음.');
+    console.log(typeof(numb) + ' = ' + numb);
+    if(numb > 0){
+        alert('양수입니다.');
+    }else if(numb < 0){
+        alert('음수입니다.');
+    }else{
+        alert('값 똑바로 넣어라. 응? 0이냐?');
+    }
+}
+// todo. 다음 세 과목의 점수를 입력받고 평균을 구한 후 수,우,미,양,가를 출력하라.
+function avgScore(){
+    
+    alert('점수똑바로 입력하라.');
+    alert('평균' + avg + '점 \n' + '수 입니다. 특급이구먼');
+    alert('평균' + avg + '점 \n' + '우 입니다. 우수하구먼');
+    alert('평균' + avg + '점 \n'  + '미 입니다. 괜찮구먼');
+    alert('평균' + avg + '점 \n'  + '양 입니다. 망했구먼');
+    alert('평균' + avg + '점 \n'  + '가 입니다. 맞겠구먼');
 }
