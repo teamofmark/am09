54b7534c6642501c59359e0f405bd663e819b795
Mark <teamofmark@gmail.com>
2023-05-22 10:50:24 +0900

    과제배포

--- JS/step3/js/3_for.js
+++ JS/step3/js/3_for.js
@@ -204,3 +204,31 @@ function runBreak(){
         }
     }
 }
+// ! 다중 for (for inner for...)
+
+// *case.1 반절 피라미드
+function halfPyramid(){
+    var star = '';
+    for(var i = 1; i <= 10; i++){
+        for(var j = 0; j < i; j++){
+            star += '*';
+        }
+        star += '<br>';
+    }
+    document.write(star);
+}
+// todo. 역반절 피라미드
+function reverseHalfPyramid(){
+    var star = '';
+    for (var i = 10; i>=1; i--){
+        for (var j = 0; j < i; j++){
+            star+='*';
+        }
+        star += '<br>';
+    }
+    document.write(star);
+}
+// todo. 완성형피라미드 만들어오기
+function pyramid(){
+    
+}
