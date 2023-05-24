8571dcf8f5fe89c3eecaf6963f487a75ab4ffa57
Mark <teamofmark@gmail.com>
2023-05-24 09:09:57 +0900

    과제풀이 종료

--- JS/step3/js/1_while.js
+++ JS/step3/js/1_while.js
@@ -35,5 +35,16 @@ function gugudan(){
 }
 //todo.2 end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하고, prompt에는 몇번 수행 했는가에 대한 "x 번째 입력" 이 표시되야함. 
 function sumNumb(){
-    
+    var i = 1; //? count Var
+    var result = 0; //? result Var
+    while(true){
+        var numb = window.prompt(i + '번째 입력');
+        if(numb == "end"){
+            alert('입력이 종료되었다?');
+            break;
+        }
+        result += parseInt(numb);
+        document.write("현재까지의 총합 : " + result + "<br>");
+        i++;
+    }
 }
