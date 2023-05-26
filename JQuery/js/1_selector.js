0ca93b6f3a5a88ded2b35ed151e9daa9c63f0d68
Mark <teamofmark@gmail.com>
2023-05-26 09:10:10 +0900

    실습문제풀이

--- JQuery/js/1_selector.js
+++ JQuery/js/1_selector.js
@@ -4,8 +4,25 @@ $(document).ready(function(){
     
     // *case. 2
     $("#btnCheck").click(sayHello);
+    // *case. 3
+    $("#btnAddBorder").click(addBorder);
+    // todo.
+    $("#btnTextChange").click(textChange);
 });
 // *case. 2
 function sayHello(){
     alert("안녕? 또왔니?");
+}
+// *case. 3
+function addBorder(){
+    $("#panel").css("border","5px solid black");
+}
+
+// todo
+function textChange(){
+    $("#panel2").css({
+        "font-size": "25px",
+        "color" : "green",
+        "font-weight" : "bold"
+    }); //? n개의 대상을 넣어야하는 경우
 }
