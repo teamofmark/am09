fc6d1ad304e833ae7d4af6af68236c95e794db84
Mark <teamofmark@gmail.com>
2023-05-26 10:18:20 +0900

    case.2 풀이완료

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     // *case. 1
     $("#btnMoveCircle").click(moveCircle);
+    // *case. 2
+    $("#btnDetailMove").click(detailMoveCircle);
 });
 // *case. 1
 function moveCircle(){
@@ -13,4 +15,23 @@ function moveCircle(){
     }else{
         alert("하여간 말 참 안들어.");
     }
+}
+// *case. 2
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val(); //? target.val(); = target의 value를 가져와라. //? target.val('blah'); = target의 value를 blah로 바꿔라. 
+    var ypos = $("#ypos").val();
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+
+    if(xpos <= 380 && ypos <= 380 && xpos >= 0 && ypos >= 0){ //? && -> true condition.
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }else{
+        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하세요.");
+    }
+    // todo. || -> false condition .
+
 }
