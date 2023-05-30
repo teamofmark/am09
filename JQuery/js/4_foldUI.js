2f98f002c26c355b439c907732a4f300336d6539
Mark <teamofmark@gmail.com>
2023-05-30 10:05:51 +0900

    foldUI 실습문제풀이

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -24,5 +24,37 @@ function imgAlignHori(){
 
 // todo. 
 function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
 
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        var imgHeight = img.height();
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos= (i%3)*imgWidth; //? 0 = 0, 1 = 1, 2 = 2 / 3 = 0
+            ypos= parseInt(i/3)*imgHeight; //? 0 = 0, 1 = 0, 2 = 0 / 3 = 1 ~~~ / 6 = 2 ~~~ / 9 = 3~~~~
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    // ? xpos
+    // ? 0(0), 1(1), 2(2),
+    // ? 3(0), 4(1), 5(2),
+    // ? 6(0), 7(1), 8(2),
+
+    // ? ypos
+    // ? 0(0), 1(0), 2(0),
+    // ? 3(1), 4(1), 5(1),
+    // ? 6(2), 7(2), 8(2),
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left": 0,
+            "top": 0
+        })
+    });
 }
