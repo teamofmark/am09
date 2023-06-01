b80898a77ee851fafeecfd8193e9bc07d6436441
Mark <teamofmark@gmail.com>
2023-06-01 10:54:15 +0900

    randomBanner만들기

--- JQuery/js/12_randomBanner.js
+++ JQuery/js/12_randomBanner.js
@@ -1,1 +1,23 @@
 var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07.jpg","logo_08.jpg"];
+var currentIndex1 = 0;
+var currentIndex2 = 0;
+var speed = 0;
+var $banner1 = null;
+var $banner2 = null;
+
+$(function(){
+    init();
+    swap($banner1,1000,currentIndex1);
+    swap($banner2,3000,currentIndex2);
+});
+function init(){
+    $banner1 = $("#banner1");
+    $banner2 = $("#banner2");
+}
+function swap(selector, speed, targetIndex){
+    setInterval(function(){
+        targetIndex = Math.floor(Math.random()*logos.length);
+        var imgName = "images/" + logos[targetIndex];
+        selector.attr("src",imgName); //? attr() -> attr("") -> 하나만 / attr ("",val) -> 하나의값을교체.
+    },speed);
+}
