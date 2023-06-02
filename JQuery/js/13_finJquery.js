bfd2a3e6d17734d3dc27a81f10434f4f2431434f
Mark <teamofmark@gmail.com>
2023-06-02 09:57:17 +0900

    jquery 최종예제 todo

--- JQuery/js/13_finJquery.js
+++ JQuery/js/13_finJquery.js
@@ -11,6 +11,7 @@ var timerID = -1;
 var $info = null;
 var goalLine = 0;
 var $fishWidth = 0;
+
 $(document).ready(function(){
     init();
     fishStartPosition();
@@ -31,3 +32,45 @@ function fishStartPosition(){
         });
     }
 }
+function initEvent(){
+    $("#start").click(startGame);
+}
+function startGame(){
+    if(timerID == -1){
+        timerID = setInterval(function(){
+            updateFishPosition(); // todo. 물고기가 실제로 이동하는 함수
+            displayFishPositionInfo(); // todo. 이동한 것을 근거로 출력하는 함수.
+            checkGoalFish();
+        },200);
+    }
+}
+function updateFishPosition(){
+    console.log("1. 물고기 움직인다.");
+} // todo. 물고기 이동 함수
+
+function displayFishPositionInfo(){
+    console.log("2. 물고기 이동정보 출력중...");
+}// todo. 물고기 이동값 출력 함수
+
+function checkGoalFish(){
+    var winnerList = [];
+    for (var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){ //? goalLine을 한마리라도 넘으면 winnerList배열에 물고기들위치저장.
+            winnerList.push({
+                index: (i), position:fishCurrentPosition
+            });
+        }
+    }
+    if(winnerList.length>0){ //? 배열에 값이 담기고 나면
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position 값을 기준 하는 내림정렬.
+        });
+        endGame();// todo. 게임종료 함수 만들기.
+        alert("우승!" + winnerList[0].index + '번 물고기!');
+    }
+}
+function endGame(){
+    console.log("4. 게임 종료. 우리수업도 종료.");
+
+}//? 게임 종료 함수.
