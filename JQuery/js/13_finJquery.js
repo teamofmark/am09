2e1e63606fb3f8d29f73b9994a51d13c29d2157c
Mark <teamofmark@gmail.com>
2023-06-02 09:29:33 +0900

    물고기 배치

--- JQuery/js/13_finJquery.js
+++ JQuery/js/13_finJquery.js
@@ -6,3 +6,28 @@
     * 물고기는 실제 경주 게임처럼 어떤 물고기가 1등이 될 것인지 예측할 수 없게 할 것.
     * 물고기가 골인 지점을 통과하면 몇 번째 물고기가 우승했는지 알려주고 게임을 종료.
 */
+var $fishList = null;
+var timerID = -1;
+var $info = null;
+var goalLine = 0;
+var $fishWidth = 0;
+$(document).ready(function(){
+    init();
+    fishStartPosition();
+    initEvent();
+});
+function init(){
+    $fishList = $(".fish");
+    $info = $("#info");
+    $fishWidth = $(".fish img").width();
+    goalLine = $("#bar").position().left  - $fishWidth;
+}
+function fishStartPosition(){
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        $fish.css({
+            left: 0,
+            top: 100 + (i*150)
+        });
+    }
+}
