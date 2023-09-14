6fa4efa049c19786476d8dab2c710f737b3d010b
Mark <teamofmark@gmail.com>
2023-09-14 10:27:46 +0900

    whileloop 과제 배포

--- JS/step3/js/1_while.js
+++ JS/step3/js/1_while.js
@@ -39,3 +39,32 @@ function whileContinue(){
     }
     document.write('최종 i = ' + i + '<br>'); //?
 }
+// *case. 4 구구단 숫자를 입력받아 출력하는 while문
+// ? 1. 숫자입력받을 prompt 출력.
+// ? 2. 숫자를 입력받으면 해당 구구단을 출력.
+// ! 9단 이내로만 입력받기. ex> 9단이 넘어갈 시 '9단 이하로 입력하세요.' 출력.
+function gugudan(){
+    var guguNumb = window.prompt('구구단숫자를 입력(1~9)');
+    var i = 1;
+    if(guguNumb <= 9){
+        while(i <= 9){
+            document.write(guguNumb + 'x' + i + '=' + guguNumb*i + '<br>');
+            i++;
+        }
+    }else{
+        alert('9단이하로만입력해라.');
+    }
+    // while(i <= 9){
+    //     if(guguNumb > 9){
+    //         alert('9단이하로만입력해라.');
+    //         break;
+    //     }
+    //     document.write(guguNumb + 'x' + i + '=' + guguNumb*i + '<br>');
+    //     i++;
+    // }
+}
+//todo. end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하고, prompt에는 몇번 수행 했는가에 대한 "x 번째 입력" 이 표시되야함. 
+
+function sumNumb(){
+    
+}
