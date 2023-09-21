e1fb7c223d2e2738f759065099dacb9a97a120de
Mark <teamofmark@gmail.com>
2023-09-21 09:52:09 +0900

    값가져오기 성공 마저 구현해보기

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("#btnMoveCircle").click(moveCircle);
+    $("#btnDetailMove").click(detailMoveCircle);
 });
 // *case. 1 원 움직이기 (xAxis)
 function moveCircle(){
@@ -13,4 +14,22 @@ function moveCircle(){
         alert("잘못된 수치입니다.");
     }
 
+}
+// *case. 2 원 움직이기 (x,y Axis)
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    //! 대상.val() -> 대상의 값을 가져와라.
+    //! 대상.val("값") -> 대상의 값을 변경해라.
+    // console.log("xpos = " + typeof(xpos) + xpos + "/" + "ypos = " + typeof(ypos) + ypos);
+
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+
+    // todo. 조건식을 완성시켜 x,y축 둘다 0미만 or 380초과를 막고 정상 이동될수있게 처리.
+    // ! 초과될시 "잘못된 수치입니다. 0 ~ 380 이내로만 입력하세요." 경고창.
+    if(){
+
+    }
 }
