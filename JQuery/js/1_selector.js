0f562df7acb954d1a3cd9307195d38f905b69df3
Mark <teamofmark@gmail.com>
2023-09-21 09:13:55 +0900

    panel 내 글자 크기 굵기 색상변경하기 실습

--- JQuery/js/1_selector.js
+++ JQuery/js/1_selector.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     colorChange();
     $("#btnHello").click(sayHello);
+    $("[id^='btnAdd']").click(addBorder);
+    // $("#btnAddBorder2").click(addBorder);
 });
 
 function colorChange(){
-    var $divs = $("div"); //? 변수에 $를 붙히는건 선생님습관.
+    var $divs = $("div:not(#panel)"); //? 변수에 $를 붙히는건 선생님습관.
     $divs.css("color","#f00"); //? jquery css 함수로 스타일적용.
 }
 function sayHello(){
     alert('hello!');
+}
+function addBorder(){
+    $("#panel").css("border","5px solid black");
 }
