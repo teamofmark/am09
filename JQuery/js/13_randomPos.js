96fc5b320336da879d2f3a05212e9384354b942e
Mark <teamofmark@gmail.com>
2023-09-27 10:51:40 +0900

    scoreCount / gameEnd 함수 정리전

---
+++ JQuery/js/13_randomPos.js
@@ -0,0 +1,42 @@
+var count = 0; //? 점수
+var $score = null; //? 전광판
+var $circle = null; //? 클릭대상
+var $btnStart = null //? 시작버튼
+var $panel = null //? stage
+var playState = false; //? 가능여부변수
+var timerID = 0;
+
+$(function(){
+    init();
+    initEvent();
+});
+function init(){
+    $score = $("#score");
+    $circle = $("#circle");
+    $btnStart = $("#btn_start");
+    $panel = $(".panel");
+}
+function initEvent(){
+    $btnStart.click(gameStart);
+    $circle.click(scoreCount);
+}
+function gameStart(){
+    playState = true;
+    if(playState == true){
+        gameEnd();
+
+        timerID = setInterval(moveCircle,500);
+    }
+}
+function moveCircle(){
+    $circle.css({
+        left: Math.floor(Math.random()*($panel.width() - $circle.width())),
+        top:Math.floor(Math.random()*($panel.height() - $circle.height()))
+    });
+}
+function scoreCount(){
+
+}
+function gameEnd(){
+
+}
