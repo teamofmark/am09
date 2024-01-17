edff440cf2982f221532ee0c7b856c660aba7103
Mark <teamofmark@gmail.com>
2024-01-17 10:22:19 +0900

    lotto과제 -> switch 로 변환

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -4,3 +4,36 @@
 switch는 조건식의 결과가 case에 걸리는 경우, 비교할때 사용한다.
 ( >,<,>=,<=,!= 와 같은 연산자를 사용 안했을 경우에만)
 */
+// *case.1 기본문법 - 홀,짝구분
+function switchGrammar(){
+    var input = Number(prompt('숫자를 입력하라', '숫자'));
+    switch(input%2){
+        case 0:
+            alert("짝수입니다.");
+            break;
+        case 1:
+            alert("홀수입니다.");
+            break;
+        default:
+            alert("숫자가 아닙니다.");
+            break;
+    }
+}
+// *case.2 lotto 과제를 switch로
+function lotto(){
+    var numbValue = window.prompt("입력하실번호는?");
+    switch(numbValue){
+        case "1":
+            alert("1등 10억!");
+            break;
+        case "2":
+            alert("2등 5천만원!");
+            break;
+        case "3":
+            alert("3등 300만원!");
+            break;
+        default:
+            alert("낙첨되었습니다.");
+            break;
+    }
+}
