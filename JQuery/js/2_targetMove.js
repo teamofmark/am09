177d520552a1c0a174172a51368597e5bd1384ab
Mark <teamofmark@gmail.com>
2024-01-25 10:47:14 +0900

    keyboard로 움직임은 성공 / 제한 두기 과제

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     // *case. 1 xAxis 원움직이기
     $("#btnMoveCircle").click(moveCircle);
+
+    // *case. 2 x/yAxis 원움직이기
+    $("#btnDetailMove").click(detailMoveCircle);
+
+    // *case. 3 방향키조작
+    keyControl();
 });
 function moveCircle(){
     // ? 선언
@@ -15,4 +21,54 @@ function moveCircle(){
     }else{
         alert("잘못된 수치입니다.");
     }
+}
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    // ?선언
+
+    // todo 처리
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+
+    if(xpos <= 380 && ypos <= 380 && xpos >= 0 && ypos >= 0){
+        // todo 출력
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }else{
+        alert("잘못된 수치입니다. 0 ~ 380 이내로 입력하시오.");
+    }
+
+}
+function keyControl(){
+    var $circle = $(".circleKey");
+    var range = 50;
+    var currentXpos = 0;
+    var currentYpos = 0;
+
+    $(document).keydown(function(e){
+        console.log("입력한 keyCode : " + typeof(e.keyCode) + e.keyCode);
+        // * w: 87(Ypos-), d: 68(Xpos+), s: 83(Ypos+), a: 65(Xpos-)
+        switch(e.keyCode){
+            case 87:
+                currentYpos -= range; // todo
+                break;
+            // todo
+            case 68:
+                currentXpos += range;
+                break;
+            case 83:
+                currentYpos += range;
+                break;
+            case 65:
+                currentXpos -= range;
+                break;
+        }
+
+        $circle.css("left",currentXpos);
+        $circle.css("top",currentYpos);
+    });
 }
