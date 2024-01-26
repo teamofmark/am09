7b91319c2975b28afc2e2ea8652fa05f799c2553
Mark <teamofmark@gmail.com>
2024-01-26 10:40:20 +0900

    add/Remove

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -40,6 +40,11 @@ function toggleTodo(){
 
 function tabUI(){
     $(".tabMenu li").click(function(){
-        
+        var activeTab = "#" + $(this).attr("data-tabNumb");
+        $(".tabMenu li").removeClass("activated");
+        $(this).addClass("activated");
+        $(".tabPage").removeClass("activated");
+        $(activeTab).addClass("activated");
+        // console.log(activeTab);
     });
 }
