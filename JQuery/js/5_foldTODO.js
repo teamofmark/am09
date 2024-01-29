c5ae0c02162bcfbc198a9cbeefd965e0e25e66bd
Mark <teamofmark@gmail.com>
2024-01-29 10:24:21 +0900

    fold 과제 관련 주석

---
+++ JQuery/js/5_foldTODO.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    // todo case.1 parameter를 이용한 방법. - click이 일어날 때 해당 열,너비,높이를 함수에 전달.
+    // todo case.2 parameter를 사용하지 않고 한번에 해결하는 방법. - .replace(); 함수 찾아적용.
+});
