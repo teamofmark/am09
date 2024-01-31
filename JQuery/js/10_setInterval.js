a84d2a7ef011734b0a3ca69ff9ad13d6b01f3b6a
Mark <teamofmark@gmail.com>
2024-01-31 09:28:46 +0900

    interval 관련 문제 힌트까지 같이 배포

---
+++ JQuery/js/10_setInterval.js
@@ -0,0 +1,20 @@
+// todo 1. 필요한 전역변수 선언
+
+$(document).ready(function(){
+    // todo 2. 변수선언 함수 실행
+    // todo 3. 이벤트 함수 실행
+});
+
+// todo 2. 변수선언 함수 생성
+
+// todo 3. 이벤트 함수 생성
+    // todo 3-1. #btn_start를 click 하였을 때 움직임 시작 함수 실행
+    // todo 3-2. #btn_stop을 click 하였을 때 멈추는 함수 실행
+
+
+// todo 3-1 움직임 시작 함수 생성
+    // todo 3-1-1 일정 시간마다 움직임 함수 실행
+
+// todo 3-1-1 움직임 함수 생성
+
+// todo 3-2 멈추는 함수 생성
