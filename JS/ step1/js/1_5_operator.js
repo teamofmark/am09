2d32769d017c3414628696d06a81c9fa889eb40e
markStark <teamofmark@gmail.com>
2024-05-31 10:56:58 +0900

    연산자 과제

--- JS/ step1/js/1_5_operator.js
+++ JS/ step1/js/1_5_operator.js
@@ -44,3 +44,72 @@
     ? 접근 연산자
     * .
 */
+// *case.1 10이 저장된 변수a, 숫자 20, 그리고 30을 더해서 result 변수에 저장후 출력.
+function numbOP1(){
+    var a = 10;
+    var result = a + 20 + 30;
+    console.log('합산 : ' + result);
+}
+// *case.2 50이 저장된 변수a, 20이 저장된 변수 b를 곱한 값을 10으로 나누어 result 변수에 저장후 출력.
+function numbOP2(){
+    var a = 50;
+    var b = 20;
+    var result = a*b/10;
+    console.log('복합연산 : ' + result);
+}
+// todo.1 숫자 5를 2로 나누었을 때 나머지 값이 몇인지 result 변수에 저장후 console에 출력.
+function numbOP3(){
+    var result = 5%2;
+    console.log(result);
+
+}
+// todo.2 'hi'가 담긴 변수 a 와 'WEB'이 담긴 변수 b를 합쳐 'hiWEB을 console에 출력.
+function stringOP(){
+     var a = 'hi',b = 'WEB';
+     console.log(a + b);
+}
+// *case.3 ul, li 구조의 tag를 html에 삽입 후 출력.
+function operatorEx(){
+    var list = '';
+    list += '<ul>';
+    list += '   <li>Hello?</li>';
+    list += '   <li>JavaScript!</li>';
+    list += '</ul>';
+    document.body.innerHTML = list;
+}
+// *case.4 연산자가 뒤로 올 경우
+function numberPlusAfter(){
+    var number = 10; //? 변수 선언 및 초기화.
+    alert(++number); // todo. 예측한 값을 적기. 11(x) - 10
+    alert(++number); // todo. 예측한 값을 적기. 12(x) - 11
+    alert(++number); // todo. 예측한 값을 적기. 13(x) - 12
+    console.log(number);
+}
+// todo. 증감연산자복합.
+function numberPlusTodo(){
+    var number = 10; //? 변수 선언 및 초기화
+    console.log(number++); // ? 출력 : 10 / 현재 : 11 
+    console.log(--number); // ? 출력 : 10 / 현재 : 10
+    console.log(++number); // ? 출력 : 11 / 현재 : 11
+    console.log(number--); // ? 출력 : 11 / 현재 : 10
+    console.log(number++); // ? 출력 : 10 / 현재 : 11
+    console.log(++number); // ? 출력 : 12 / 현재 : 12
+    console.log(--number); // ? 출력 : 11 / 현재 : 11
+    console.log(number++); // ? 출력 : 11 / 현재 : 12
+    console.log(++number); // ? 출력 : 13 / 현재 : 13 
+    console.log(number); //? 13.
+}
+// * case. 5 숫자 10이 담긴 변수a, 숫자 20이 담긴 변수b의 크기를 비교하여 결과를 출력.
+function compareOperator1(){
+    var a = 10;
+    var b = 20;
+    var result = a<b;
+    console.log("a > b = " + result);
+}
+// * case. 6 a변수에는 '여자', b변수에는 '웹디자이너'라는 값이 저장되어 있다. 담겨있는 두 값이 모두 맞는지 result변수에 저장 후 출력.
+function logicalOperator1(){
+    var a = "여자";
+    var b = "웹디자이너";
+    var result = (a=="여자")&&(b=="웹디자이너");
+    console.log("result = " + result);
+}
