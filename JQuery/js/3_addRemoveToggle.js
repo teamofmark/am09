bc3acd419aa4fac3b00178cb49e3251bde02a867
markStark <teamofmark@gmail.com>
2024-06-20 10:49:56 +0900

    과제배포

---
+++ JQuery/js/3_addRemoveToggle.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    toggleCheck();
+    toggleTodo();
+});
+function toggleCheck(){
+    var $checkTarget = $(".material-icons.checkIco");
+    var $checkLabel = $(".label.checkLabel");
+    var checkStatus = false;
+
+    $checkLabel.click(function(){
+        checkStatus = !checkStatus;
+        if(checkStatus == true){
+            $checkTarget.text("check_box");
+        }else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+    });
+}
+
+function toggleTodo(){
+    // todo. 1 사용할 변수 선언
+
+    // todo. 2 처리(이벤트 > 바뀌었을 때(after) : 바뀌기 전(before))
+        // todo. 3 icon이름 찾아서 text함수로 변경 / type 변경 -> .attr()
+        // ? attr -  attr(); - 모든 attr반환. / attr(attrName) - 지정된 attr값 불러오기 / attr(attrName,attrValue) - 지정된 attr의 값을 value로 바꿔라.
+}
