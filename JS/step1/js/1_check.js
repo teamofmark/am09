233e9b6e8615ce0eb8d93384e37b617a0b050fb0
markStark <teamofmark@gmail.com>
2024-11-04 10:31:01 +0900

    궁금한 부분을 물어보는 방법 3가지

---
+++ JS/step1/js/1_check.js
@@ -0,0 +1,9 @@
+function runAlert(){
+    alert('2nd. alert!');
+}
+function runWrite(){
+    document.write('hello' + '<br>' + 'js...!');
+}
+function runLog(){
+    console.log('궁금한 부분을 넣으면 나옴.');
+}
