3b7191696a4edaa1f3d50449f8b31de06fd1226e
markStark <teamofmark@gmail.com>
2024-11-11 10:48:32 +0900

    성적등급산출

--- JS/step2/js/1_if.js
+++ JS/step2/js/1_if.js
@@ -47,3 +47,50 @@ function lastDayEx(){
         
     console.log(endOfMonth(dt).toString());
 }
+// todo. 현재 시간이 본인 기준으로 아침? 점심? 저녁을 먹을 시간인지 알려주는 함수 만들기
+function mealTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    if(hour < 11){
+        alert('아침먹을시간');// 각 조건 상황에 따라 해당 구문이 출력 될 수 있도록 하기.
+    }else if(hour < 15){
+        alert('점심먹을시간');// 각 조건 상황에 따라 해당 구문이 출력 될 수 있도록 하기.
+    }else{
+        alert('저녁먹을시간');// 각 조건 상황에 따라 해당 구문이 출력 될 수 있도록 하기.
+    }
+}
+// *case. 3 숫자판별
+function numbJudgment(){
+    var numb = prompt('숫자를 입력하세요. 양,음수를 구분합니다.');
+    console.log(typeof(numb) + ' = ' + numb);
+    numb = parseFloat(numb);
+    console.log(typeof(numb) + ' = ' + numb);
+    if(numb > 0){
+        alert('양수입니다.');
+    }else if(numb < 0){
+        alert('음수입니다.');
+    }else{
+        alert('입력된 값이 0이거나 올바르지 않습니다.');
+    }
+}
+// todo. 입력된 숫자가 홀수인지 짝수인지 판별하는 조건문 완성하기
+function oddOrEven(){
+    var numb = prompt('숫자를 입력하세요. 홀,짝수를 구분합니다.');
+    
+    if(numb%2==0){
+        alert('짝수입니다.'); //? 짝수조건을 만들어서 출력
+    }else if(numb%2==1){
+        alert('홀수입니다.'); //? 홀수조건을 만들어서 출력
+    }else{
+        alert('올바른 값을 입력하세요.'); //? 오류상황에 따른 메시지 생각하여 기술하기.
+    }
+}
+// case. 4 다음 세과목의 점수를 입력받고 평균을 구한 뒤 수,우,미,양,가 등급으로 출력하라.
+function avgScore(){
+    var koreanScore = prompt('국어 점수를 입력하시오.');
+    var englishScore = prompt('영어 점수를 입력하시오.');
+    var mathScore = prompt('수학 점수를 입력하시오.');
+    var avg = (Number(koreanScore) + Number(englishScore) + Number(mathScore))/3;
+    console.log(avg);
+    // 조건문 완성 예정
+}
