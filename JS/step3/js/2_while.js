1aa9477c964152c137489d8c67985426f009d119
markStark <teamofmark@gmail.com>
2024-11-19 10:11:34 +0900

    while 과제 배포 및 함수시작

--- JS/step3/js/2_while.js
+++ JS/step3/js/2_while.js
@@ -1,9 +1,66 @@
 /*
     ? while
     while 반복문은 조건이 참일 경우 무한 반복한다.
-    (물론 조건이 정해진다면 for문 처럼도 사용 가능하지만, 
-        용도를 구분지어 사용하는 것이 좋다.)
+    (물론 조건이 정해진다면 for문 처럼도 사용 가능하지만, 용도를 구분지어 사용하는 것이 좋다.)
     정해진 횟수만큼의 반복은 for를 사용하지만, 무한반복처리, 파일 읽고쓰기 및 
     전송(java,php...), DB데이터 출력하기.
 
 */
+// case.1 password '1234'가 입력되면 '접속승인', 아니면 '잘못입력하였습니다.'
+function pwCheck(){
+    while(true){
+        var value = prompt("패스워드를 입력하세요.");
+        if(value==1234){
+            alert('접속승인!');
+            break;
+        }else{
+            alert("잘못입력하였습니다.");
+        }
+    }
+}
+// case.2 whileContinue
+function whileContinue(){
+    var i = 1;
+    while(i<=10){ //? 총 10회 반복
+        i++; //? i 초기값부터 10번증가
+        continue; //? for문과 마찬가지로 continue로 인해 이하 로직수행불가.
+        document.write(i + '<br>');
+    }
+    document.write("최종 i =" + i + '<br>');
+}
+// case.3 while Break
+function whileBreak(){
+    var i = 1;
+    while(i<=10){
+        i++;
+        break;
+        document.write(i + '<br>');
+    }
+    document.write("최종 i = " + i + "<br>");
+}
+
+// todo 구구단 출력 while loop.
+// ! 1 ~ 9 까지만 입력될 수 있게 / 이상이 입력될 경우 '9단 이하로 입력하세요' 메시지 출력
+function gugudan(){
+    var guguNumb = prompt('구구단 숫자를 입력(1~9까지)');
+    var i = 1;
+    if(guguNumb <= 9){
+        while(i <= 9){
+            document.write(guguNumb + ' x ' + i + ' = ' + guguNumb * i + '<br>');
+            i++;
+        }
+    }else{
+        alert('9단 이하로 입력하세요.');
+    }
+    /*
+    while(i <= 9){
+        if(guguNumb > 9){
+            alert('9단 이하로 입력하세요');
+            break;
+        }
+        document.write(guguNumb + ' x ' + i + ' = ' + guguNumb * i + '<br>');
+        i++;
+    }*/
+}
+// todo. end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하기.
+// ! prompt에는 실행한 횟수가 출력되야함(x번째 입력중)
