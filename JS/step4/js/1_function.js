fcde7c2faf681c86577d08151279334d914639b7
markStark <teamofmark@gmail.com>
2024-11-21 09:38:09 +0900

    1차 - 매개변수를통한 계산기

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -115,7 +115,7 @@ function infiniteSum(){
     }
     document.write("총 "+count+"번 실행함.");
 }
-// csae.7 무한곱셈공식 생성 - 어느 곳에서나 출력이 가능한.
+// case.7 무한곱셈공식 생성 - 어느 곳에서나 출력이 가능한.
 function printGugudan(numb){
     var data = '';
     for(var i = 2; i<=numb; i++){
@@ -126,4 +126,42 @@ function printGugudan(numb){
         data += "<br>";
     }
     return data;
+}
+
+
+// todo. 다음실행구문으로 전달받은 매개변수로 계산하여 결과를 출력하는 함수 만들기.
+/*
+    //* 실행구문
+    document.write("1 결과 =" + calculator("+", 20, 10) + "<br>");
+    document.write("2 결과 =" + calculator("-", 20, 10) + "<br>");
+    document.write("3 결과 =" + calculator("*", 20, 10) + "<br>");
+    document.write("4 결과 =" + calculator("/", 20, 10) + "<br>");
+    document.write("5 결과 =" + calculator("%", 20, 10) + "<br>");
+    //* 결과 
+    결과 = 30
+    결과 = 10
+    결과 = 200
+    결과 = 2
+    결과 = 잘못된 연산자 입니다.
+*/
+function calculator(op,numb1,numb2){
+    var result = '';
+    switch(op){
+        case "+":
+            result = numb1 + numb2;
+            break;
+        case "-":
+            result = numb1 - numb2;
+            break;
+        case "*":
+            result = numb1 * numb2;
+            break;
+        case "/":
+            result = numb1 / numb2;
+            break;
+        default:
+            result = "잘못된 연산자입니다.";
+            break;
+    }
+    return result;
 }
