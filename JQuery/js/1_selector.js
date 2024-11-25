41d1cd10b63ec299dbd2b4c7cadb7408ce588f0b
markStark <teamofmark@gmail.com>
2024-11-25 10:36:48 +0900

    간단한 선택자들을 이용한 css

--- JQuery/js/1_selector.js
+++ JQuery/js/1_selector.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
     txtColorSwap();
     // case.2
     $("#btnCheck").click(sayHello);
+    // case.3
+    $("#btnAddBorder").click(addBorder);
+    // $("#btnAddBorder").click(function(){
+    //     $("#panel").css("border","5px solid black");
+    // });
+    // todo
+    $("#btnTextChange").click(textChange);
 });
 // case.1
 function txtColorSwap(){
@@ -12,4 +19,17 @@ function txtColorSwap(){
 // case.2
 function sayHello(){
     alert('안녕?');
+}
+// case.3
+function addBorder(){
+    $("#panel").css("border","5px solid black");
+}
+// todo
+function textChange(){
+    $("#panel2").css({
+        "font-size":"25px",
+        "color":"green",
+        "font-weight":"bold",
+        "line-height":"31px"
+    });
 }
