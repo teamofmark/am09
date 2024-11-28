64b8e730d71cf1b1125ca82d00987bb43fba8bec
markStark <teamofmark@gmail.com>
2024-11-28 10:18:36 +0900

    열,행 배치

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     // case.1 xAxis 로 imgs 펼치기
     imgAlignHori();
+    // todo. x,yAxis 로 imgs 펼치기
+    imgAlignMix();
 });
 // case.1
 function imgAlignHori(){
@@ -18,5 +20,30 @@ function imgAlignHori(){
     });
     $(".reset").click(function(){
         img.css("left",0);
+    });
+}
+// todo
+function imgAlignMix(){
+    var img = $(".imgContainerMix img");
+    var xpos = 0;
+    var ypos = 0;
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgHeight = img.height();
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%3)*imgHeight; //? 0,1,2,0,1,2... -> 3을 넘지 못한다. (ex> 3/3 = 0)
+            ypos = parseInt(i/3)*imgHeight; //? 0,0,0,1,1,1,2,2,2.... -> 3배수관계일때 1씩 증가
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+    $(".resetMix").click(function(){
+        img.css({
+            "left" : 0,
+            "top" : 0
+        })
     });
 }
