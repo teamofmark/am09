b318d129578c24b8586af62f26658c050c04cbee
Mark <teamofmark@gmail.com>
2025-05-14 10:44:15 +0900

    toString 진수변환개념 예

--- JS/step1/js/4_typeofchange.js
+++ JS/step1/js/4_typeofchange.js
@@ -37,7 +37,7 @@ function forcedString2(){
 }
 // *case.5 test1에 들어있는 숫자를 문자로 명시적 형변환.
 /*
-    ? String
+    ? String()
     * String(value); - 문자형으로 변환
 
     ? .toString()
@@ -46,3 +46,29 @@ function forcedString2(){
     ? .toFixed()
     * Number.toFixed(); - (소수점자리수(반올림발생))실수문자형 변환.
 */
+function stringNumb(){
+    var test = 15;
+    console.log("변환 전 : " + typeof(test) + ' = ' + test);
+    var result = String(test);
+    console.log("변환 후 : " + typeof(result) + ' = ' + result);
+}
+function stringtoNumb(){
+    var test = 15;
+    console.log("변환 전 : " + typeof(test) + ' = ' + test);
+    var result = test.toString();
+    console.log("변환 후 : " + typeof(result) + ' = ' + result);
+}
+// case.5 - 1 randomBackgroundColor
+function bgColorRandom(){
+    setInterval(() =>{
+        var bgColor = Math.random()*0xfff;
+        // ? 0x = 16진을 의미 = 15*16^3 + 15*16^2 +  15*16^1 + 15*16^0 - 최대수치를 입력한것
+        // ! 2진 표현시 0000 0000 0000 0000 1111 1111 1111 1111 -> 실사용불가 -> 변환.
+        console.log(bgColor);
+        bgColor = parseInt(bgColor);
+        console.log(bgColor);
+        bgColor = bgColor.toString(16);
+        console.log(bgColor);
+        document.querySelector("body").style.backgroundColor="#"+bgColor;
+    },1000);
+}
