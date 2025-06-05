dcb51f860219b830a17d343f8d867155eeeb0c03
Mark <teamofmark@gmail.com>
2025-06-05 10:52:33 +0900

    foldUI 제작

---
+++ JQuery/js/4_foldUI2.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    imgAlignCustom(3);
+});
+function imgAlignCustom(count){
+    var img = $(".imgContainerCustom img");
+    var xpos = 0;
+    var ypos = 0;
+    $(".alignMix").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        var imgHeight = img.height();
+
+        for(var i = 0; i < imgLength; i++){
+            var image = img.eq(i);
+            xpos = (i%count)*imgWidth;
+            ypos = parseInt(i/count)*imgHeight;
+            image.css({
+                "left" : xpos,
+                "top" : ypos
+            });
+        }
+    });
+}
