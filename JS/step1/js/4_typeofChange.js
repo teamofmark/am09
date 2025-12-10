0185dcd8a3b4e32c1938aabcf44e317f4982ddbe
Mark <teamofmark@gmail.com>
2025-12-10 10:32:56 +0900

    문자형변환 문제 풀이

--- JS/step1/js/4_typeofChange.js
+++ JS/step1/js/4_typeofChange.js
@@ -79,3 +79,44 @@ function fixtoNumb(){
     var result = pi.toFixed(2);
     console.log("변환 후 : " + typeof(result) + ' = ' + result);
 }
+// *case. 6 test에 들어있는 문자를 숫자로 형변환.
+/*
+    ? Number
+    * Number(value); - 정수, 실수 구분없이 형변환
+
+    ? parse
+    * 1. parseInt(value); - 정수형변환 (올림처리없음)
+    * 2. parseFloat(value); - 실수형변환
+*/
+// *case. 6-1
+function numbString(){
+    var test = '300.15625';
+    console.log('변환 전 : ' + typeof(test) + ' = ' + test);
+    var result = parseInt(test) + 10;
+    console.log('변환 후 : ' + typeof(result) + ' = ' + result);
+}
+// todo.1 변수 test에 들어있는 숫자 100을 문자형으로 변형.(각기다른) 출력문(typeof검수-위 예제들처럼)
+// todo. 1-1 암시적형변환 방식
+function todo1(){
+    var test = 100;
+    console.log('변환 전 : ' + typeof(test) + ' = ' + test);
+        //todo 암시적형변환 - 숫자와 문자가 더해졌을 때 -> 문자가됨
+    var result = test + "";
+    console.log('변환 후 : ' + typeof(result) + ' = ' + result);
+}
+// todo. 1-2 String 형변환
+function todo2(){
+    var test = 100;
+    console.log('변환 전 : ' + typeof(test) + ' = ' + test);
+        //todo 전역함수형변환
+    var result = String(test);
+    console.log('변환 후 : ' + typeof(result) + ' = ' + result);
+}
+// todo. 1-3 toString 형변환
+function todo3(){
+    var test = 100;
+    console.log('변환 전 : ' + typeof(test) + ' = ' + test);
+        //todo 꼬리함수형변환
+    var result = test.toString();
+    console.log('변환 후 : ' + typeof(result) + ' = ' + result);
+}
