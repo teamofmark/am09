9bcb682533d127eb0b07e95f3e2bae14c353dce6
Mark <teamofmark@gmail.com>
2025-12-10 09:18:59 +0900

    암시적형변환

--- JS/step1/js/4_typeofChange.js
+++ JS/step1/js/4_typeofChange.js
@@ -18,3 +18,20 @@ function agePlus(){
     var result = Number(age) + 20;
     console.log(result);
 }
+// case.2 강제 숫자 - 문자형 변환.
+function forcedString(){
+    var a = '30';
+    var result = 1+ a +10; // 기존 언어 방식들에선 대부분 err, 반면 js는 암시적형변환 개념으로 인해. 강제변환.
+    console.log(result);
+}
+// case.3 강제 불린 - 숫자형 변환.
+function forcedNumber(){
+    var result =  2 + true;
+    console.log("result의 자료유형은" + typeof(result) + "값은" + result);
+}
+// case.4 강제 불린 - 문자형 변환.
+function forcedString2(){
+    var a  = '2';
+    var result = a + true;
+    console.log("result의 자료유형은" + typeof(result) + "값은" + result);
+}
