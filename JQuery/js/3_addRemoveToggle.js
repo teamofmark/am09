0fb3aaa6ef683ef3da43cb872fdde65e25c9e973
Mark <teamofmark@gmail.com>
2026-01-05 10:05:11 +0900

    type attr 사용

--- JQuery/js/3_addRemoveToggle.js
+++ JQuery/js/3_addRemoveToggle.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     toggleCheck();
+    toggleTodo();
 });
 function toggleCheck(){
     var $checkTarget = $(".material-icons.checkIco");
@@ -14,4 +15,22 @@ function toggleCheck(){
             $checkTarget.text("check_box_outline_blank");
         }
     });  
+}
+// todo. password 만들어보기
+// ! html대상의 속성을 변경하거나 불러오는 함수는 attr(); 함수이다.
+function toggleTodo(){
+    var $toggleTarget = $(".material-icons.toggleVisible");
+    var $toggleInput = $("#userPW");
+    var toggleStatus = false;
+
+    $toggleTarget.click(function(){
+        toggleStatus = !toggleStatus;
+        if(toggleStatus == true){
+            $toggleTarget.text('visibility');
+            $toggleInput.attr("type","text");
+        }else{
+            $toggleTarget.text('visibility_off');
+            $toggleInput.attr("type","password");
+        }
+    });
 }
