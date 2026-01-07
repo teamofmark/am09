9e48af0e760a76c7f1b18da922702b40d8dd3ebe
Mark <teamofmark@gmail.com>
2026-01-07 09:16:36 +0900

    풀이완료

--- JQuery/js/8_setTimeout.js
+++ JQuery/js/8_setTimeout.js
@@ -7,7 +7,7 @@ var $resetBtn = null;
 $(document).ready(function(){
     init();
     scoreCount(count, $score, $circle, playState);
-
+    $resetBtn.click(restart);
 });
 function init(){
     $score = $("#score");
@@ -17,7 +17,20 @@ function init(){
 function scoreCount(count, $score, $circle, playState){
     alert("게임시작!");
     $circle.click(function(){
-        count++;
-        $score.text("현재 점수는 " + count + "점 입니다.");
+        if(playState == true){
+            count++;
+            $score.text("현재 점수는 " + count + "점 입니다.");
+        }
     });
+    setTimeout(function(){
+        alert("게임종료.");
+        playState = false;
+    },5000);
+    // setTimeout([function],[time])
+}
+function restart(){
+    playState = true;
+    count = 0;
+    $score.text("현재 점수는 " + count + "점 입니다.");
+    scoreCount(count, $score, $circle, playState);
 }
