0bdeba8d1c0790a069999580faebc949347156b4
Mark <teamofmark@gmail.com>
2026-01-08 09:10:53 +0900

    random처리방식 에대한 추가설명

--- JQuery/js/12_random.js
+++ JQuery/js/12_random.js
@@ -29,8 +29,7 @@ function swap(selector, speed, targetIndex){
         ! random method : 매개변수 = x, 리턴값 = 0 - 1 사이 소수값.
         ? 1. random 메서드는 0 - 1 범위에서 0 과 1을 포함하지 않는 소수를 랜덤하게 리턴한다. (0.111111111~0.999999999)
         ? 2. parseInt는 안의 수를 소수에서 정수로 변환한다.
-        ? 3. random*50 이 되면 결과적으로 0 ~ 50 사이의 정수가 나오게 된다.(parseInt) 
-        ? 4. 예를 들어 random 에서 0.9938 을 리턴했다면 * 50 이 되면서 49.69 가 되고 parseInt로 인해 49가 된다. 그 이후 50이 더해지며 99가 된다.
+        ? 3. 예를 들어 random 에서 0.9938 을 리턴했다면 * 50 이 되면서 49.69 가 되고 parseInt로 인해 49가 된다. 
         */
         var imgName = "images/" + logos[targetIndex];
         selector.attr("src", imgName);
