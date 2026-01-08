a327fa57d687b3d70211657ecbec526ca8b70007
Mark <teamofmark@gmail.com>
2026-01-08 10:18:07 +0900

    물고기움직이기 완성

--- JQuery/js/13_finJquery.js
+++ JQuery/js/13_finJquery.js
@@ -39,7 +39,7 @@ function startGame(){
         timerID = setInterval(function(){
             updateFishPosition();// ? 물고기움직이기
             displayFishPositionInfo();// ? 움직인 물고기 위치 표시
-            // ? checkGoalFish(); 결승선 도달 감지
+            checkGoalFish();// ? 결승선 도달 감지
         },200);
         console.log("4. 게임 정상 시작.")
     }
@@ -63,4 +63,37 @@ function displayFishPositionInfo(){
     }
     $info.html(info);
     console.log("6. 물고기 위치 표시 정상.");
+}
+//  ? 결승선 도달 감지 함수
+function checkGoalFish(){
+    var winnerList = [];
+    for (var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position: fishCurrentPosition
+                // 지정된 인덱스(index:번호)에 레이블(position):값(fishCurrentPosition)으로 저장.
+            });
+            console.log("7. 데이터 저장 성공.");
+        }
+    }
+    // ? 세 마리중 한마리라도 결승선 도달시 물고기 위치 저장.
+    if(winnerList.length > 0){
+        winnerList.sort(function(a,b){
+            return b.position - a.position; //? position 값 기준 내림차순정렬
+        });
+        /*
+            오름차순 -> 진행될수록 점점 값이 커지거나 (1,2,3,4....) 정순으로 (ex> a,b,c,d....) 정렬
+            내림차순 -> 진행될수록 점점 값이 내려가거나 (...4,3,2,1) 역순으로 (ex> ....d,c,b,a) 정렬
+        */
+       endGame();
+       console.log("8. 우승 물고기 선별 완료.");
+       alert("우승!" + winnerList[0].index+"번 물고기!");
+    }
+}
+// ? 게임 종료 함수
+function endGame(){
+    clearInterval(timerID);
+    timerID = -1;
+    console.log("9. 게임 종료.");
 }
