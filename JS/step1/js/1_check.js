d8d898c5b57804b9972de3ff83395ba66b5382f1
Mark <teamofmark@gmail.com>
2026-04-23 10:49:12 +0900

    check 방식

---
+++ JS/step1/js/1_check.js
@@ -0,0 +1,9 @@
+function runAlert(){
+    alert("안녕?"); // libFunction - 라이브러리함수 = 내가만든 함수가 아님. 원래 있는 것.
+}
+function runWrite(){
+    document.write('hi');
+}
+function runLog(){
+    console.log('곤방와');
+}
