1e0e7ca3167bdee8d80f7f62b8b12a8ad46845d0
Mark <teamofmark@gmail.com>
2026-05-06 10:50:02 +0900

    for loop 진입

--- JS/step3/js/1_for.js
+++ JS/step3/js/1_for.js
@@ -14,6 +14,53 @@
 
 for (var index = 0; index < 10; index++) {
     console.log(index);
-    
 }
 */
+// case.1 이름을 10번 출력해보자.
+function namePrint10(){
+    var userName = "Mark";
+    for(var i =  1; i <= 10; i++){
+        document.write(i+". "+userName + "<br>");
+    }
+}
+// case.2 이름을 1,000번 출력해보자.
+function namePrint1000(){
+    var userName = "Mark";
+    for(var i = 1; i <= 1000; i++){
+        document.write(i + '. ' + userName + "<br>");
+    }
+}
+// todo. 1,000번 출력중 홀수번째에만 이름이 출력되게 만들기
+function namePrint500(){
+    var userName = "Mark";
+    for(var i = 1; i<= 1000; i+=2){
+        document.write(i + '. ' + userName + "<br>");
+    }
+}
+// case.3 JS Engine처럼 생각하기
+function thinkJS(){
+    for(var i = 0; i < 10; i++){ //? i가 10보다 작을때 까지만 반복을 수행.
+        document.write("i = " + i, "<br>"); //? 위 조건이 참일 경우 실행한다 = 0~9까지 출력.
+    }//? i가 증가하다가 10이 되는 순간 break.
+    document.write("종료 i = " + i ); //? for loop가 중지될 때 i의 값인 10이 출력.
+}
+// todo. 1-10까지 출력되는 단순 반복문을 여러개 만들어보기
+function answer1(){
+    for(var i = 1; i<=10; i+=2){ //?10보다 작거나 같을때 (1~10)까지만 반복 수행. 단, 반복이 끝난후 i에 2를 더함.
+        document.write(i+"<br>"); //? 증감이 이루어 지지 않은 1,3,5,7,9
+        document.write((i+1)+"<br>"); //? 증감이 이루어진 2,4,6,8,10
+    }
+}
+function answer2(){
+    for(var i = 10; i > 0; i--){
+        document.write((11 - i) + "<br>");
+        console.log(i);
+        continue;
+        document.write(i+"<br>");
+    }
+}
+function answer3(){
+    for(var i = 10; i >= 1; i--){
+        document.write((11- i ) + "<br>");
+    }
+}
