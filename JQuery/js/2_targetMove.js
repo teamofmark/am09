2fe7126f4622a1395e163904179841af2db0f940
Mark <teamofmark@gmail.com>
2026-05-26 10:52:26 +0900

    x,y축움직임구현

--- JQuery/js/2_targetMove.js
+++ JQuery/js/2_targetMove.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $("#btnMoveCircle").click(moveCircle);
+    // $("#btnDetailMove").click(detailMoveCircle);
+    detailMoveCircleExt();
 });
 function moveCircle(){
     var $circle = $(".circle");
@@ -9,5 +11,48 @@ function moveCircle(){
         $circle.css("left",xpos);
     }else{
         alert("잘못된 수치입니다.");
+    }
+}
+// todo. x,y축 움직임 주기 : 380 제한
+function detailMoveCircle(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+    if(xpos <= 380 && ypos <= 380 && xpos >= 0 && ypos >= 0){
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }else{
+        alert("잘못된 수치입니다. 0 ~ 380이내로 입력하세요.");
+    }
+}
+
+function detailMoveCircleExt(){
+    var $circle = null;
+    circleInit();
+    $("#btnDetailMove").click(circleEvent);
+}
+function circleInit(){
+    $circle = $(".circleDetail");
+}
+function circleEvent(){
+    var $circle = $(".circleDetail");
+    var xpos = $("#xpos").val();
+    var ypos = $("#ypos").val();
+    xpos = parseInt(xpos);
+    ypos = parseInt(ypos);
+    circleMovCommand(xpos,ypos);
+}
+function circleMovCommand(xpos, ypos){
+    if(xpos <= 380 && ypos <= 380 && xpos >= 0 && ypos >= 0){
+        $circle.css({
+            "left" : xpos,
+            "top" : ypos
+        });
+    }else{
+        alert("잘못된 수치입니다. 0 ~ 380이내로 입력하세요.");
     }
 }
