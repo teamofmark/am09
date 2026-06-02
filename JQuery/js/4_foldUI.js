d1404b62dabf590c1962a7b555d7e56964635aec
Mark <teamofmark@gmail.com>
2026-06-02 10:16:11 +0900

    실습문제구조

--- JQuery/js/4_foldUI.js
+++ JQuery/js/4_foldUI.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     imgAlignHori();
-    imgAlignMix();
+    imgAlignMix(3);
 });
 // case. 1 x축 정렬
 function imgAlignHori(){
@@ -20,8 +20,8 @@ function imgAlignHori(){
     });
 }
 
-// todo. x,y축으로 이미지 배치하기 (3열x3행 배치)
-function imgAlignMix(){
+// todo. x,y축으로 이미지 배치하기 (3열x3행 배치) + 매개변수형태변환
+function imgAlignMix(count){
     var img = $(".imgContainerMix img");
     var xpos = 0;
     var ypos = 0;
@@ -31,8 +31,8 @@ function imgAlignMix(){
         var imgHeight = img.height();
         for(var i = 0; i < imgLength; i++){
             var image = img.eq(i);
-            xpos = parseInt(i%3) * imgHeight; //? 0/3 = 나머지 0, 1/3 = 1, 2/3 = 2, 3/3 = 0, 3/4 = 1, 3/5 = 2, 3/6 = 0....
-            ypos = parseInt(i/3) * imgHeight; //? 0/3 = 0, 1/3 = 0, 2/3 = 0, 3/3 = 1, 3/4 = 1.xxxx, 3/5 = 1.xxxx, 3/6 = 2
+            xpos = parseInt(i%count) * imgHeight; //? 0/3 = 나머지 0, 1/3 = 1, 2/3 = 2, 3/3 = 0, 3/4 = 1, 3/5 = 2, 3/6 = 0....
+            ypos = parseInt(i/count) * imgHeight; //? 0/3 = 0, 1/3 = 0, 2/3 = 0, 3/3 = 1, 3/4 = 1.xxxx, 3/5 = 1.xxxx, 3/6 = 2
             image.css({
                 "left" : xpos,
                 "top" : ypos
