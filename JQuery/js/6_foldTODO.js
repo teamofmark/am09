a6d87feabdc75f29f5e3386bcb547628f84d1b14
Mark <teamofmark@gmail.com>
2026-06-03 09:40:17 +0900

    3차처리

--- JQuery/js/6_foldTODO.js
+++ JQuery/js/6_foldTODO.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    gallery();
+    /*
     $(".1way").click(function(){gallery2(1)});
     $(".2way").click(function(){gallery2(2)});
     $(".3way").click(function(){gallery2(3)});
@@ -9,6 +11,7 @@ $(document).ready(function(){
     $(".8way").click(function(){gallery2(8)});
     $(".9way").click(function(){gallery2(9)});
     $(".10way").click(function(){gallery2(10)});
+    */
 });
 function gallery2(count){
     var $images=$("#imgContainer img");
@@ -29,4 +32,29 @@ function gallery2(count){
             "height" : imgH
         })
     }
+}
+function gallery(){
+    var currentWay,xpos,ypos;
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var containerW = $("#imgContainer").width();
+    var imgSize = $images.width();
+    $("[class$='way']").click(function(){
+        currentWay = parseInt($(this).attr("class").replace("way",""));
+        // console.log("현재 가져온 값은 : " + typeof(currentWay) + "자료형의 " + currentWay + "다.");
+        for(var i = 0; i < $imgLength; i++){
+            // todo
+            var $image = $images.eq(i);
+            imgSize = containerW / currentWay;
+            xpos = (i % currentWay) * imgSize;
+            ypos = parseInt(i / currentWay) * imgSize;
+
+            $image.css({
+                'left' : xpos,
+                'top' : ypos,
+                'width' : imgSize,
+                'height' : imgSize
+            })
+        }
+    });
 }
