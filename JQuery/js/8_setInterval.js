429cfc8536b20467960bb014f52796ba2d27d4a3
Mark <teamofmark@gmail.com>
2026-06-04 09:44:49 +0900

    원형메트로놈

--- JQuery/js/8_setInterval.js
+++ JQuery/js/8_setInterval.js
@@ -13,7 +13,7 @@ function init(){
 }
 function initEvent(){
     $("#btn_start").click(moveStart); //? 움직임
-    $("#btn_stop").click(); //? 중지
+    $("#btn_stop").click(moveStop); //? 중지
 }
 function moveStart(){
     if(timerID == 0){
@@ -23,4 +23,15 @@ function moveStart(){
 }
 function moveCircle(){
     // ? circle position 움직임 구현 예정
+    var xpos = $circle.position().left;
+    xpos += runStep;
+    $circle.css("left",xpos);
+    // todo. 사각형 프레임 안에 원이 가둬져서 반복운동 하게 만들기
+    if(xpos > railWidth || xpos < 0){
+        runStep *= -1;
+    }
+}
+function moveStop(){
+    clearInterval(timerID); //? setInterval kill -> parameter값으로 전달해야함.
+    timerID = 0;
 }
